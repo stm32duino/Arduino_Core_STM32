66d9693fbfc52380fe1d7652b237d876659e6d19
Frederic Pillon <frederic.pillon@st.com>
2017-06-12 20:41:14 +0200

    Start adding NUCLEO_F030R8
    Copy NUCLE0_F091RC to NUCLEO_F030R8
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

---
+++ variants/NUCLEO_F030R8/variant.cpp
@@ -0,0 +1,168 @@
+/*
+  Copyright (c) 2011 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "variant.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Pin number
+const PinName digital_arduino[] = {
+  PA3,  //D0
+  PA2,  //D1
+  PA10, //D2
+  PB3,  //D3
+  PB5,  //D4
+  PB4,  //D5
+  PB10, //D6
+  PA8,  //D7
+  PA9,  //D8
+  PC7,  //D9
+  PB6,  //D10
+  PA7,  //D11
+  PA6,  //D12
+  PA5,  //D13
+  PB9,  //D14
+  PB8,  //D15
+// ST Morpho
+// CN7 Left Side
+  PC10, //D16
+  PC12, //D17
+  PF11, //D18 - BOOT0
+  PA13, //D19 - SWD
+  PA14, //D20 - SWD
+  PA15, //D21
+  PB7,  //D22
+  PC13, //D23
+  PC14, //D24
+  PC15, //D25
+  PF0,  //D26
+  PF1,  //D27
+  PC2,  //D28
+  PC3,  //D29
+// CN7 Right Side
+  PC11, //D30
+  PD2,  //D31
+// CN10 Left Side
+  PC9,  //D32
+// CN10 Right side
+  PC8,  //D33
+  PC6,  //D34
+  PC5,  //D35
+  PA12, //D36
+  PA11, //D37
+  PB12, //D38
+  PB11, //D39
+  PB2,  //D40
+  PB1,  //D41
+  PB15, //D42
+  PB14, //D43
+  PB13, //D44
+  PC4,  //D45
+  PA0,  //D46/A0
+  PA1,  //D47/A1
+  PA4,  //D48/A2
+  PB0,  //D49/A3
+  PC1,  //D50/A4
+  PC0,  //D51/A5
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * UART objects
+ */
+HardwareSerial  Serial(PA3, PA2); //Connected to ST-Link
+HardwareSerial  Serial1(PA10, PA9);
+HardwareSerial  Serial2(PA1, PA0);
+
+// Need rework to be generic
+
+void serialEvent() __attribute__((weak));
+void serialEvent() { }
+void serialEvent1() __attribute__((weak));
+void serialEvent1() { }
+void serialEvent2() __attribute__((weak));
+void serialEvent2() { }
+
+void serialEventRun(void)
+{
+  if (Serial.available()) serialEvent();
+  if (Serial1.available()) serialEvent1();
+  if (Serial2.available()) serialEvent2();
+}
+
+// ----------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __libc_init_array(void);
+
+uint32_t pinNametoPinNumber(PinName p)
+{
+  uint32_t i = 0;
+  for(i = 0; i < NUM_DIGITAL_PINS; i++) {
+	  if (digital_arduino[i] == p)
+		  break;
+  }
+  return i;
+}
+
+void init( void )
+{
+  hw_config_init();
+}
+
+/**
+  * @brief  System Clock Configuration
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              | RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ variants/NUCLEO_F030R8/variant.h
@@ -0,0 +1,177 @@
+/*
+  Copyright (c) 2011 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _VARIANT_ARDUINO_STM32_
+#define _VARIANT_ARDUINO_STM32_
+
+/*----------------------------------------------------------------------------
+ *        Definitions
+ *----------------------------------------------------------------------------*/
+
+/** Frequency of the board main oscillator */
+//#define VARIANT_MAINOSC		12000000
+
+/** Master clock frequency */
+//#define VARIANT_MCK			84000000
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "Arduino.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif // __cplusplus
+
+/**
+ * Libc porting layers
+ */
+#if defined (  __GNUC__  ) /* GCC CS3 */
+#    include <syscalls.h> /** RedHat Newlib minimal stub */
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Pins
+ *----------------------------------------------------------------------------*/
+#include "PeripheralPins.h"
+
+extern const PinName digital_arduino[];
+
+enum {
+  D0,  D1,  D2,  D3,  D4,  D5,  D6,  D7,  D8,  D9,
+  D10, D11, D12, D13, D14, D15, D16, D17, D18, D19,
+  D20, D21, D22, D23, D24, D25, D26, D27, D28, D29,
+  D30, D31, D32, D33, D34, D35, D36, D37, D38, D39,
+  D40, D41, D42, D43, D44, D45, D46, D47, D48, D49,
+  D50, D51,
+  DEND
+};
+
+enum {
+  A_START_AFTER = D45,
+  A0,  A1,  A2,  A3,  A4,  A5,
+  AEND
+};
+
+#define MAX_ANALOG_IOS          (sizeof(PinMap_ADC)/sizeof(PinMap))
+#define MAX_DIGITAL_IOS         DEND
+#define NUM_DIGITAL_PINS        MAX_DIGITAL_IOS
+#define NUM_ANALOG_INPUTS       (AEND - A0)
+
+// Convert a digital pin number Dxx to a PinName Pxy
+#define digitalToPinName(p)     ((p < NUM_DIGITAL_PINS) ? digital_arduino[p] : (STM_VALID_PINNAME(p))? (PinName)p : NC)
+// Convert an analog pin number Axx to a PinName Pxy
+#define analogToPinName(p)      (digitalToPinName(p))
+// Convert an analog pin number to a digital pin number
+#define analogToDigital(p)      (p)
+// Convert a PinName Pxy to a pin number
+uint32_t pinNametoPinNumber(PinName p);
+
+#define digitalPinToPort(p)     ( get_GPIO_Port(digitalToPinName(p)) )
+#define digitalPinToBitMask(p)  ( STM_GPIO_PIN(digitalToPinName(p)) )
+
+//ADC resolution is 12bits
+#define ADC_RESOLUTION          12
+#define DACC_RESOLUTION         12
+
+//PWR resolution
+#define PWM_RESOLUTION          8
+#define PWM_FREQUENCY           1000
+#define PWM_MAX_DUTY_CYCLE      255
+
+//On-board LED pin number
+#define LED_BUILTIN             13
+#define LED_GREEN               LED_BUILTIN
+
+//On-board user button
+#define USER_BTN                23
+
+
+//SPI definitions
+//define 16 channels. As many channel as digital IOs
+#define SPI_CHANNELS_NUM        16
+
+//default chip salect pin
+#define BOARD_SPI_DEFAULT_SS    10
+
+//In case SPI CS channel is not used we define a default one
+#define BOARD_SPI_OWN_SS        SPI_CHANNELS_NUM
+
+#define SS                      BOARD_SPI_DEFAULT_SS
+#define SS1                     4
+#define SS2                     7
+#define SS3                     8
+#define MOSI                    11
+#define MISO                    12
+#define SCLK                    13
+#define SCK                     SCLK
+
+//I2C Definitions
+#define SDA                     14
+#define SCL                     15
+
+//Timer Definitions
+//Do not use timer used by PWM pins when possible. See PinMap_PWM.
+#define TIMER_TONE              TIM6
+#define TIMER_UART_EMULATED     TIM7
+
+//Do not use basic timer: OC is required
+#define TIMER_SERVO             TIM2  //TODO: advanced-control timers don't work
+
+#define DEBUG_UART              ((USART_TypeDef *) USART2)
+
+// UART Emulation
+#define UART_EMUL_RX            PB14
+#define UART_EMUL_TX            PB13
+
+//Enable Firmata
+#define STM32 1
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern HardwareSerial Serial;
+extern HardwareSerial Serial1;
+extern HardwareSerial Serial2;
+
+// These serial port names are intended to allow libraries and architecture-neutral
+// sketches to automatically default to the correct port name for a particular type
+// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+// the first hardware serial port whose RX/TX pins are not dedicated to another use.
+//
+// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+//
+// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+//
+// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+//
+// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+//
+// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+//                            pins are NOT connected to anything by default.
+#define SERIAL_PORT_MONITOR   Serial
+#define SERIAL_PORT_HARDWARE  Serial
+#endif
+
+#endif /* _VARIANT_ARDUINO_STM32_ */
