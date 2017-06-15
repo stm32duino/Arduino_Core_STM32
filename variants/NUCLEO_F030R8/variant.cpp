91aab461ecf95de25bc78f29ccc6befc4ed58a9f
Frederic.Pillon <frederic.pillon@st.com>
2017-06-15 08:41:05 +0200

    Update SystemClock_Config
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- variants/NUCLEO_F030R8/variant.cpp
+++ variants/NUCLEO_F030R8/variant.cpp
@@ -153,7 +153,9 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct;
 
   /* No HSE Oscillator on Nucleo, Activate PLL with HSI/2 as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 15;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
