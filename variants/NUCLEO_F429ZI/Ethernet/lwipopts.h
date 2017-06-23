5a48f790a1d85176a3a8fd2fbd5d85c4fad54ac8
fpr <fabien.perroquin@wi6labs.com>
2017-06-23 12:06:11 +0200

    Removed debug options of LwIP stack to decrease code size and increase execution speed.
    Signed-off-by: fpr <fabien.perroquin@wi6labs.com>

--- variants/NUCLEO_F429ZI/Ethernet/lwipopts.h
+++ variants/NUCLEO_F429ZI/Ethernet/lwipopts.h
@@ -47,41 +47,6 @@
 #ifndef __LWIPOPTS_H__
 #define __LWIPOPTS_H__
 
-#define LWIP_DEBUG    LWIP_DBG_ON
-// #define NETIF_DEBUG   LWIP_DBG_ON
-// #define IP_DEBUG      LWIP_DBG_ON
-// #define UDP_DEBUG     LWIP_DBG_ON
-// #define DHCP_DEBUG    LWIP_DBG_ON
-// #define AUTOIP_DEBUG  LWIP_DBG_ON
-
-// #define ETHARP_DEBUG  LWIP_DBG_ON
-// #define PBUF_DEBUG  LWIP_DBG_ON
-// #define API_LIB_DEBUG  LWIP_DBG_ON
-// #define API_MSG_DEBUG  LWIP_DBG_ON
-// #define SOCKETS_DEBUG  LWIP_DBG_ON
-// #define ICMP_DEBUG  LWIP_DBG_ON
-// #define IGMP_DEBUG  LWIP_DBG_ON
-// #define INET_DEBUG  LWIP_DBG_ON
-// #define IP_REASS_DEBUG  LWIP_DBG_ON
-// #define RAW_DEBUG  LWIP_DBG_ON
-// #define MEM_DEBUG  LWIP_DBG_ON
-// #define MEMP_DEBUG  LWIP_DBG_ON
-// #define SYS_DEBUG  LWIP_DBG_ON
-// #define TIMERS_DEBUG  LWIP_DBG_ON
-// #define TCP_DEBUG  LWIP_DBG_ON
-// #define TCP_INPUT_DEBUG  LWIP_DBG_ON
-// #define TCP_FR_DEBUG  LWIP_DBG_ON
-// #define TCP_RTO_DEBUG  LWIP_DBG_ON
-// #define TCP_CWND_DEBUG  LWIP_DBG_ON
-// #define TCP_WND_DEBUG  LWIP_DBG_ON
-// #define TCP_OUTPUT_DEBUG  LWIP_DBG_ON
-// #define TCP_RST_DEBUG  LWIP_DBG_ON
-// #define TCP_QLEN_DEBUG  LWIP_DBG_ON
-// #define TCPIP_DEBUG  LWIP_DBG_ON
-// #define SLIP_DEBUG  LWIP_DBG_ON
-// #define DNS_DEBUG  LWIP_DBG_ON
-// #define IP6_DEBUG  LWIP_DBG_ON
-
 /**
  * NO_SYS==1: Provides VERY minimal functionality. Otherwise,
  * use lwIP facilities.
