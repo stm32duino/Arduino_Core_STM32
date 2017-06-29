bc20b9e7fa9b7fad7f8ebe065e4a4aff3da7d06a
Frederic.Pillon <frederic.pillon@st.com>
2017-06-29 08:43:32 +0200

    Fix typo
    Clean comment
    
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/digital_io.c
+++ cores/arduino/stm32/digital_io.c
@@ -35,17 +35,6 @@
   *
   ******************************************************************************
   */
-/** @addtogroup CMSIS
-  * @{
-  */
-
-/** @addtogroup stm32f4xx_system
-  * @{
-  */
-
-/** @addtogroup STM32F4xx_System_Private_Includes
-  * @{
-  */
 #include "digital_io.h"
 #include "stm32_def.h"
 #include "hw_config.h"
@@ -53,49 +42,6 @@
 #ifdef __cplusplus
  extern "C" {
 #endif
-/**
-  * @}
-  */
-
-/** @addtogroup STM32F4xx_System_Private_TypesDefinitions
-  * @{
-  */
-
-/**
-  * @}
-  */
-
-/** @addtogroup STM32F4xx_System_Private_Defines
-  * @{
-  */
-/**
-  * @}
-  */
-
-/** @addtogroup STM32F4xx_System_Private_Macros
-  * @{
-  */
-
-/**
-  * @}
-  */
-
-/** @addtogroup STM32F4xx_System_Private_Variables
-  * @{
-  */
-
-/**
-  * @}
-  */
-
-/** @addtogroup STM32F4xx_System_Private_FunctionPrototypes
-  * @{
-  */
-
-/**
-  * @}
-  */
-
 
 /**
   * @brief  This function initialize the IO
@@ -108,7 +54,7 @@
 void digital_io_init(PinName pin, uint32_t mode, uint32_t pull)
 {
   GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_TypeDef *port = set_GPIO_Port_Clock(STM_PORT(pin));;
+  GPIO_TypeDef *port = set_GPIO_Port_Clock(STM_PORT(pin));
   GPIO_InitStructure.Pin = STM_GPIO_PIN(pin);
   GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStructure.Mode = mode;
@@ -144,17 +90,6 @@ uint32_t digital_io_read(GPIO_TypeDef  *port, uint32_t pin)
   return (uint32_t)HAL_GPIO_ReadPin(port, pin);
 }
 
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
 #ifdef __cplusplus
 }
 #endif
