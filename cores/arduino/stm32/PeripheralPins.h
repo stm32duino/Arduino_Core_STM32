65c31262be8ee0ec3d08e1c234394ea55d7732e0
fpr <fabien.perroquin@wi6labs.com>
2017-07-04 10:25:26 +0200

    Adds Ethernet pins configuration
    Signed-off-by: fpr <fabien.perroquin@wi6labs.com>

--- cores/arduino/stm32/PeripheralPins.h
+++ cores/arduino/stm32/PeripheralPins.h
@@ -62,5 +62,8 @@ extern const PinMap PinMap_SPI_SSEL[];
 extern const PinMap PinMap_CAN_RD[];
 extern const PinMap PinMap_CAN_TD[];
 
+//*** ETHERNET ***
+extern const PinMap PinMap_Ethernet[];
+
 #endif
 
