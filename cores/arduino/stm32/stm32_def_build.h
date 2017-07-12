997c4134ac450e685914f271a009624e6d20454d
Frederic.Pillon <frederic.pillon@st.com>
2017-07-12 14:49:18 +0200

    Add CMSIS_STARTUP_FILE definition
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/stm32_def_build.h
+++ cores/arduino/stm32/stm32_def_build.h
@@ -26,6 +26,8 @@
 #define CMSIS_STARTUP_FILE "startup_stm32f091xc.s"
 #elif defined(STM32F030x8)
 #define CMSIS_STARTUP_FILE "startup_stm32f030x8.s"
+#elif defined(STM32F207xx)
+#define CMSIS_STARTUP_FILE "startup_stm32f207xx.s"
 #elif defined(STM32F303xE)
 #define CMSIS_STARTUP_FILE "startup_stm32f303xe.s"
 #elif defined(STM32F407xx)
