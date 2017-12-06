09e08b786c7f0666351b2e76e503914851efdde0
Frederic.Pillon <frederic.pillon@st.com>
2017-12-06 13:48:55 +0100

    Added checking gcc version
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -20,6 +20,15 @@
 #ifndef Arduino_h
 #define Arduino_h
 
+#ifndef GCC_VERSION
+#define GCC_VERSION (__GNUC__ * 10000 \
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__)
+#endif
+#if GCC_VERSION < 60300
+#error "GCC version 6.3 or higher is required"
+#endif
+
 #include "wiring.h"
 
 /* sketch */
