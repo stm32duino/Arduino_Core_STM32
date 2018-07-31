6c0595bb5acd39424899c74747c1db2a49f63b49
davidanger <davidanger@163.com>
2018-07-31 10:55:13 +0800

    [Add NUCLEO-F767ZI] Merge PinName from NUCLEO-F429ZI

--- variants/NUCLEO_F767ZI/variant.cpp
+++ variants/NUCLEO_F767ZI/variant.cpp
@@ -24,31 +24,103 @@ extern "C" {
 
 // Pin number
 const PinName digitalPin[] = {
-  PC_7,  //D0
-  PC_6,  //D1
-  PG_6,  //D2
-  PB_4,  //D3
-  PG_7,  //D4
-  PI_0,  //D5
-  PH_6,  //D6
-  PI_3,  //D7
-  PI_2,  //D8
-  PA_15, //D9
-  PA_8,  //D10
-  PB_15, //D11
-  PB_14, //D12
-  PI_1,  //D13
+  PG_9,  //D0
+  PG_14, //D1
+  PF_15, //D2
+  PE_13, //D3
+  PF_14, //D4
+  PE_11, //D5
+  PE_9,  //D6
+  PF_13, //D7
+  PF_12, //D8
+  PD_15, //D9
+  PD_14, //D10
+  PA_7,  //D11
+  PA_6,  //D12
+  PA_5,  //D13
   PB_9,  //D14
   PB_8,  //D15
-  PA_0,  //D16/A0
-  PF_10, //D17/A1
-  PF_9,  //D18/A2
-  PF_8,  //D19/A3
-  PF_7,  //D20/A4
-  PF_6,  //D21/A5
-  PI_11, //D22 User btn
-  PB_7,  //D23 ST-Link Rx
-  PA_9   //D24 ST-Link Tx
+  PC_6,  //D16
+  PB_15, //D17
+  PB_13, //D18
+  PB_12, //D19
+  PA_15, //D20
+  PC_7,  //D21
+  PB_5,  //D22
+  PB_3,  //D23
+  PA_4,  //D24
+  PB_4,  //D25
+  PB_6,  //D26
+  PB_2,  //D27
+  PD_13, //D28
+  PD_12, //D29
+  PD_11, //D30
+  PE_2,  //D31
+  PA_0,  //D32
+  PB_0,  //D33 - LED_GREEN
+  PE_0,  //D34
+  PB_11, //D35
+  PB_10, //D36
+  PE_15, //D37
+  PE_14, //D38
+  PE_12, //D39
+  PE_10, //D40
+  PE_7,  //D41
+  PE_8,  //D42
+  PC_8,  //D43
+  PC_9,  //D44
+  PC_10, //D45
+  PC_11, //D46
+  PC_12, //D47
+  PD_2,  //D48
+  PG_2,  //D49
+  PG_3,  //D50
+  PD_7,  //D51
+  PD_6,  //D52
+  PD_5,  //D53
+  PD_4,  //D54
+  PD_3,  //D55
+  PE_2,  //D56
+  PE_4,  //D57
+  PE_5,  //D58
+  PE_6,  //D59
+  PE_3,  //D60
+  PF_8,  //D61
+  PF_7,  //D62
+  PF_9,  //D63
+  PG_1,  //D64
+  PG_0,  //D65
+  PD_1,  //D66
+  PD_0,  //D67
+  PF_0,  //D68
+  PF_1,  //D69
+  PF_2,  //D70
+  PA_7,  //D71
+  NC,    //D72
+  PB_7,  //D73 - LED_BLUE
+  PB_14, //D74 - LED_RED
+  PC_13, //D75 - USER_BTN
+  PD_9,  //D76 - Serial Rx
+  PD_8,  //D77 - Serial Tx
+  PA_3,  //D78/A0
+  PC_0,  //D79/A1
+  PC_3,  //D80/A2
+  PF_3,  //D81/A3
+  PF_5,  //D82/A4
+  PF_10, //D83/A5
+  PB_1,  //D84/A6
+  PC_2,  //D85/A7
+  PF_4,  //D86/A8
+  PF_6,  //D87/A9
+  // Duplicated pins in order to be aligned with PinMap_ADC
+  PA_7,  //D88/A10 = D11
+  PA_6,  //D89/A11 = D12
+  PA_5,  //D90/A12 = D13
+  PA_4,  //D91/A13 = D24
+  PA_0,  //D92/A14 = D32
+  PF_8,  //D93/A15 = D61
+  PF_7,  //D94/A16 = D62
+  PF_9   //D95/A17 = D63
 };
 
 #ifdef __cplusplus
