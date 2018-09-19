c5017df708ca3b639fe332a9e8ca83c63f25cf40
Frederic.Pillon <frederic.pillon@st.com>
2018-09-19 14:52:30 +0200

    Remove non standard API
    delayInsideIT() code should have been in the method:
    void GSM3SoftSerial::tunedDelay(uint16_t delay)
    It was introduced as an API to support
    built-in GSM library on Nucleo-L476RG and is not relevant for other.
    
    Fix #250
    
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/clock.c
+++ cores/arduino/stm32/clock.c
@@ -88,40 +88,6 @@ void SysTick_Handler(void)
 }
 
 /**
-  * @brief  Function provides us delay (required by some arduino libraries).
-  *         Can be called inside an interrupt.
-  * @param  None
-  * @retval None
-  */
-void delayInsideIT(uint32_t delay_us)
-{
-  uint32_t nb_loop;
-#if defined (STM32F0xx) || defined (STM32L0xx)
-  nb_loop = (((HAL_RCC_GetHCLKFreq() / 1000000)/5)*delay_us)+1; /* uS (divide by 4 because each loop take about 4 cycles including nop +1 is here to avoid delay of 0 */
-  __asm__ volatile(
-  "1: " "\n\t"
-  " nop " "\n\t"
-  " sub %0, %0, #1 " "\n\t"
-  " bne 1b " "\n\t"
-  : "=r" (nb_loop)
-  : "0"(nb_loop)
-  : "r3"
-  );
-#else
-  nb_loop = (((HAL_RCC_GetHCLKFreq() / 1000000)/4)*delay_us)+1; /* uS (divide by 4 because each loop take about 4 cycles including nop +1 is here to avoid delay of 0 */
-  __asm__ volatile(
-  "1: " "\n\t"
-  " nop " "\n\t"
-  " subs.w %0, %0, #1 " "\n\t"
-  " bne 1b " "\n\t"
-  : "=r" (nb_loop)
-  : "0"(nb_loop)
-  : "r3"
-  );
-#endif
-}
-
-/**
   * @brief  Enable the specified clock if not already set
   * @param  source: clock source: LSE_CLOCK, LSI_CLOCK, HSI_CLOCK or HSE_CLOCK
   * @retval None
