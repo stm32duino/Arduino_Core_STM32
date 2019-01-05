82a7cb6301ff4c98a56851ece06cb1204bd4d218
Venelin Efremov <ghent360@iqury.us>
2019-01-05 13:33:36 -0800

    Remove the HSE_VALUE hack and update the hal_conf file directly.

--- variants/EEXTR_F030_V1/stm32f0xx_hal_conf.h
+++ variants/EEXTR_F030_V1/stm32f0xx_hal_conf.h
@@ -83,7 +83,7 @@
   *        (when HSE is used as system clock source, directly or through the PLL).
   */
 #if !defined  (HSE_VALUE)
-  #define HSE_VALUE            8000000U  /*!< Value of the External oscillator in Hz */
+  #define HSE_VALUE            16000000U  /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 /**
