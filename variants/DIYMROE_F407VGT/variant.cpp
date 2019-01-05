46b92c83d19ac740f787eb858751da1c4edbe267
BennehBoy <bennyboy@benneh.net>
2019-01-05 16:40:56 +0000

    Correct PLL Freq from 25 to 8 to fix USB

--- variants/DIYMROE_F407VGT/variant.cpp
+++ variants/DIYMROE_F407VGT/variant.cpp
@@ -184,7 +184,7 @@ WEAK void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
