60607e1c723ec7930fb73675a687662296cf6bc2
Frederic.Pillon <frederic.pillon@st.com>
2019-02-01 08:52:36 +0100

    [HWSerial] Clean header includes
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/HardwareSerial.cpp
+++ cores/arduino/HardwareSerial.cpp
@@ -22,10 +22,7 @@
   Modified 3 December 2013 by Matthijs Kooijman
 */
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
 #include "Arduino.h"
 #include "HardwareSerial.h"
 
