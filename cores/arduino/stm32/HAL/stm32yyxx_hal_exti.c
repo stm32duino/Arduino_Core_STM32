df4966143edb3811c7f94fb906193417b9ccd4fb
Frederic.Pillon <frederic.pillon@st.com>
2019-02-26 09:37:16 +0100

    [F4] Update HAL/LL wrapper files
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/HAL/stm32yyxx_hal_exti.c
+++ cores/arduino/stm32/HAL/stm32yyxx_hal_exti.c
@@ -1,3 +1,6 @@
+#ifdef STM32F4xx
+#include "stm32f4xx_hal_exti.c"
+#endif
 #ifdef STM32H7xx
 #include "stm32h7xx_hal_exti.c"
 #endif
--- cores/arduino/stm32/HAL/stm32yyxx_hal_smbus.c
+++ cores/arduino/stm32/HAL/stm32yyxx_hal_smbus.c
@@ -4,6 +4,9 @@
 #ifdef STM32F3xx
 #include "stm32f3xx_hal_smbus.c"
 #endif
+#ifdef STM32F4xx
+#include "stm32f4xx_hal_smbus.c"
+#endif
 #ifdef STM32F7xx
 #include "stm32f7xx_hal_smbus.c"
 #endif
