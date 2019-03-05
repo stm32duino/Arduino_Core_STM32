de6869230bf4a9e2b6237d53232faef18db824d2
Frederic.Pillon <frederic.pillon@st.com>
2019-03-05 08:55:51 +0100

    [I2C] Fix HAL F4
    In function:
    static HAL_StatusTypeDef I2C_Master_SB(I2C_HandleTypeDef *hi2c)
    
    This check has been added:
          if ((hi2c->hdmatx->XferCpltCallback != NULL) || (hi2c->hdmarx->XferCpltCallback != NULL))
          {
            /* Enable DMA Request */
            SET_BIT(hi2c->Instance->CR2, I2C_CR2_DMAEN);
          }
    
    But, in case of the DMA is not used, hi2c->hdmatx and hi2c->hdmarx are NULL.
    In this case, hi2c->hdma(rx|tx)->XferCpltCallback can be not NULL
    then I2C_CR2_DMAEN is set while it should not.
    As this bit is checked several time to know if DMA is used or not this
    raised an exception handler.
    
    Fix #463
    
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- system/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c
+++ system/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c
@@ -5449,7 +5449,8 @@ static void I2C_Master_SB(I2C_HandleTypeDef *hi2c)
         hi2c->Instance->DR = I2C_7BIT_ADD_READ(hi2c->Devaddress);
       }
 
-      if ((hi2c->hdmatx->XferCpltCallback != NULL) || (hi2c->hdmarx->XferCpltCallback != NULL))
+      if (((hi2c->hdmatx) && (hi2c->hdmatx->XferCpltCallback != NULL)) ||
+          ((hi2c->hdmarx) && (hi2c->hdmarx->XferCpltCallback != NULL)))
       {
         /* Enable DMA Request */
         SET_BIT(hi2c->Instance->CR2, I2C_CR2_DMAEN);
