b1fec1a8602d05f3946540cc5b715d89e7ad1b93
Frederic.Pillon <frederic.pillon@st.com>
2019-04-18 14:21:53 +0200

    [UART] Reorder serial_t field to avoid padding
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/uart.h
+++ cores/arduino/stm32/uart.h
@@ -55,8 +55,6 @@ typedef struct serial_s serial_t;
 struct serial_s {
   USART_TypeDef *uart;
   UART_HandleTypeDef handle;
-  uint8_t index;
-  uint8_t recv;
   uint32_t baudrate;
   uint32_t databits;
   uint32_t stopbits;
@@ -64,11 +62,13 @@ struct serial_s {
   PinName pin_tx;
   PinName pin_rx;
   IRQn_Type irq;
+  uint8_t index;
+  uint8_t recv;
   uint8_t *rx_buff;
-  volatile uint16_t rx_head;
-  uint16_t rx_tail;
   uint8_t *tx_buff;
+  uint16_t rx_tail;
   uint16_t tx_head;
+  volatile uint16_t rx_head;
   volatile uint16_t tx_tail;
 };
 
