a8c26db0a6e00401f706bdb8a6faf57fa8907688
Vassilis Serasidis <avrsite@yahoo.gr>
2019-06-11 13:34:53 +0300

    HID Bootloader v2.2 support

--- cores/arduino/stm32/bootloader.c
+++ cores/arduino/stm32/bootloader.c
@@ -1,6 +1,7 @@
 #include "bootloader.h"
 
 #include "stm32_def.h"
+#include "backup.h"
 
 #ifdef BL_LEGACY_LEAF
 void dtr_togglingHook(uint8_t *buf, uint32_t *len)
@@ -15,3 +16,24 @@ void dtr_togglingHook(uint8_t *buf, uint32_t *len)
   }
 }
 #endif /* BL_LEGACY_LEAF */
+
+#ifdef BL_HID
+void dtr_togglingHook(uint8_t *buf, uint32_t *len)
+{
+  /**
+   * Four byte is the magic pack "1EAF" that puts the MCU into bootloader.
+   * Check if the incoming contains the string "1EAF".
+   * If yes, put the MCU into the bootloader mode.
+   */
+  if ((*len >= 4) && (buf[0] == '1') && (buf[1] == 'E') && (buf[2] == 'A') && (buf[3] == 'F')) {
+    enableBackupDomain();
+    /* New HID Bootloader (ver 3.0+) */
+    setBackupRegister(HID_MAGIC_NUMBER_BKP_INDEX, HID_MAGIC_NUMBER_BKP_VALUE);
+#ifdef HID_OLD_MAGIC_NUMBER_BKP_INDEX
+    /* Compatibility to the old HID Bootloader (ver <= 2.1) */
+    setBackupRegister(HID_OLD_MAGIC_NUMBER_BKP_INDEX, HID_MAGIC_NUMBER_BKP_VALUE);
+#endif
+    NVIC_SystemReset();
+  }
+}
+#endif /* BL_HID */
