41eb7f0514c26781adc87f06306226e83fe1957b
Terry Moore <tmm@mcci.com>
2019-07-29 16:06:33 -0400

    Fix copyright notice

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -1,6 +1,7 @@
 /*
   Arduino.h - Main include file for the Arduino SDK
   Copyright (c) 2005-2013 Arduino Team.  All right reserved.
+  Copyright (c) 2019 MCCI Corporation. All rights reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
