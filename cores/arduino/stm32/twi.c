a8b801dedc7004251e3c69f23258a3fe70f62e26
Frederic.Pillon <frederic.pillon@st.com>
2019-08-21 14:53:31 +0200

    [I2C] Harden error management
    Example for I2C scanner fewer errors are reported.
    Sometime there is a HAL_I2C_ERROR_TIMEOUT was raised but
    not reported as I2C_TIMEOUT.
    In a general way, mask have to be used to properly handle error cases.
    
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/twi.c
+++ cores/arduino/stm32/twi.c
@@ -723,16 +723,19 @@ i2c_status_e i2c_master_write(i2c_t *obj, uint8_t dev_address,
       while ((HAL_I2C_GetState(&(obj->handle)) != HAL_I2C_STATE_READY)
              && (ret == I2C_OK)) {
         delta = (HAL_GetTick() - tickstart);
-        if (delta > I2C_TIMEOUT_TICK) {
+        uint32_t err = HAL_I2C_GetError(&(obj->handle));
+        if ((delta > I2C_TIMEOUT_TICK)
+            || ((err & HAL_I2C_ERROR_TIMEOUT) == HAL_I2C_ERROR_TIMEOUT)) {
           ret = I2C_TIMEOUT;
-        } else if (HAL_I2C_GetError(&(obj->handle)) != HAL_I2C_ERROR_NONE) {
+        } else if (err != HAL_I2C_ERROR_NONE) {
           ret = I2C_ERROR;
         }
       }
     }
     /* When Acknowledge failure occurs (Slave don't acknowledge it's address)
        Master restarts communication */
-  } while (HAL_I2C_GetError(&(obj->handle)) == HAL_I2C_ERROR_AF && delta < I2C_TIMEOUT_TICK);
+  } while (((HAL_I2C_GetError(&(obj->handle)) & HAL_I2C_ERROR_AF) == HAL_I2C_ERROR_AF)
+           && (delta < I2C_TIMEOUT_TICK));
 
   return ret;
 }
@@ -784,16 +787,19 @@ i2c_status_e i2c_master_read(i2c_t *obj, uint8_t dev_address, uint8_t *data, uin
       while ((HAL_I2C_GetState(&(obj->handle)) != HAL_I2C_STATE_READY)
              && (ret == I2C_OK)) {
         delta = (HAL_GetTick() - tickstart);
-        if (delta > I2C_TIMEOUT_TICK) {
+        uint32_t err = HAL_I2C_GetError(&(obj->handle));
+        if ((delta > I2C_TIMEOUT_TICK)
+            || ((err & HAL_I2C_ERROR_TIMEOUT) == HAL_I2C_ERROR_TIMEOUT)) {
           ret = I2C_TIMEOUT;
-        } else if (HAL_I2C_GetError(&(obj->handle)) != HAL_I2C_ERROR_NONE) {
+        } else if (err != HAL_I2C_ERROR_NONE) {
           ret = I2C_ERROR;
         }
       }
     }
     /* When Acknowledge failure occurs (Slave don't acknowledge it's address)
        Master restarts communication */
-  } while (HAL_I2C_GetError(&(obj->handle)) == HAL_I2C_ERROR_AF && delta < I2C_TIMEOUT_TICK);
+  } while (((HAL_I2C_GetError(&(obj->handle)) & HAL_I2C_ERROR_AF) == HAL_I2C_ERROR_AF)
+           && (delta < I2C_TIMEOUT_TICK));
 
   return ret;
 }
