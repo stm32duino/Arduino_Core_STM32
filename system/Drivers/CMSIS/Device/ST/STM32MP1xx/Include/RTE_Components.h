e27703350d0da6f07e9db06baf65f74c518b4dec
Bumsik Kim <k.bumsik@gmail.com>
2019-10-12 20:39:10 +0900

    [MP1] Add the first CMSIS Driver

---
+++ system/Drivers/CMSIS/Device/ST/STM32MP1xx/Include/RTE_Components.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HAL_CORE'
+ * Target:  'STM32MP1xx'
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File:
+ */
+#define CMSIS_device_header "stm32mp1xx_hal.h"
+
+#endif /* RTE_COMPONENTS_H */
