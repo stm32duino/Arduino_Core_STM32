986403710e48734e5b31bbab8a5b3bc3299bea6e
ladyada <limor@ladyada.net>
2019-10-12 23:03:04 -0400

    fixclk

--- variants/FEATHER_F405/variant.cpp
+++ variants/FEATHER_F405/variant.cpp
@@ -117,7 +117,7 @@ WEAK void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE / 1000000);
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
