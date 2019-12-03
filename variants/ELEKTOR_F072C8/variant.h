7b0bc9cb305d019436c1a6f0ff8165af6f32ac9b
lab-mathias-claussen <41676430+lab-mathias-claussen@users.noreply.github.com>
2019-12-03 07:42:40 +0100

    Update variants/ELEKTOR_F072C8/variant.h
    Co-Authored-By: Frederic Pillon <frederic.pillon@st.com>

--- variants/ELEKTOR_F072C8/variant.h
+++ variants/ELEKTOR_F072C8/variant.h
@@ -84,7 +84,6 @@ extern "C" {
 #define NUM_ANALOG_FIRST        26
 
 // Could be redefined here if needed
-#define ADC_RESOLUTION          12
 // On-board LED pin number
 #define LED_BUILTIN             PA15
 #define LED_RED                 LED_BUILTIN
