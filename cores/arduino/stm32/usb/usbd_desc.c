0e24dcb697880c64ff176c746eb6d7d541ba9438
Matthijs Kooijman <matthijs@stdin.nl>
2020-01-23 21:43:18 +0100

    [USB] Set the bcdDevice value to 0
    This value should probably be configurable by the sketch, so a
    meaningful value can be picked. As long is this is not possible, best to
    use 0, rather than 2.0, otherwise it later becomes impossible to use 2.0
    in a meaningful way (you'll never know if the device is really 2.0, or
    an old version that had no meaningful version number).

--- cores/arduino/stm32/usb/usbd_desc.c
+++ cores/arduino/stm32/usb/usbd_desc.c
@@ -135,8 +135,8 @@ __ALIGN_BEGIN uint8_t USBD_Class_DeviceDesc[USB_LEN_DEV_DESC] __ALIGN_END = {
   HIBYTE(USBD_VID),           /* idVendor */
   LOBYTE(USBD_PID),           /* idProduct */
   HIBYTE(USBD_PID),           /* idProduct */
-  0x00,                       /* bcdDevice rel. 2.00 */
-  0x02,
+  0x00,                       /* bcdDevice rel. 0.00 */
+  0x00,
   USBD_IDX_MFC_STR,           /* Index of manufacturer string */
   USBD_IDX_PRODUCT_STR,       /* Index of product string */
   USBD_IDX_SERIAL_STR,        /* Index of serial number string */
@@ -159,8 +159,8 @@ __ALIGN_BEGIN uint8_t USBD_Class_DeviceDesc[USB_LEN_DEV_DESC] __ALIGN_END = {
   HIBYTE(USBD_VID),           /* idVendor */
   LOBYTE(USBD_PID),           /* idProduct */
   HIBYTE(USBD_PID),           /* idProduct */
-  0x00,                       /* bcdDevice rel. 2.00 */
-  0x02,
+  0x00,                       /* bcdDevice rel. 0.00 */
+  0x00,
   USBD_IDX_MFC_STR,           /* Index of manufacturer string */
   USBD_IDX_PRODUCT_STR,       /* Index of product string */
   USBD_IDX_SERIAL_STR,        /* Index of serial number string */
