399f2faf0c855b25ced7a16b07026c6c05a1da75
Frederic Pillon <frederic.pillon@st.com>
2020-02-28 08:16:01 +0100

    [CI] Reduce unnecessary output from action
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- .github/actions/pio-build/entrypoint.sh
+++ .github/actions/pio-build/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 CMSIS_VERSION=$1
 CMSIS_ARCHIVE=CMSIS-${CMSIS_VERSION}.tar.bz2
@@ -11,8 +11,8 @@ python3 -c "import json; import os; fp=open(os.path.expanduser('~/.platformio/pl
 
 ln -sf $GITHUB_WORKSPACE $HOME/.platformio/packages/framework-arduinoststm32
 # Download and unpack CMSIS package
-wget https://github.com/stm32duino/ArduinoModule-CMSIS/releases/download/$CMSIS_VERSION/CMSIS-$CMSIS_VERSION.tar.bz2
-tar -xvjf CMSIS-$CMSIS_VERSION.tar.bz2
+wget --no-verbose https://github.com/stm32duino/ArduinoModule-CMSIS/releases/download/$CMSIS_VERSION/CMSIS-$CMSIS_VERSION.tar.bz2
+tar -xjf CMSIS-$CMSIS_VERSION.tar.bz2
 cd $GITHUB_WORKSPACE/CI/build/
 python3 platformio-builder.py --board=blackpill_f103c8 --board=remram_v1
 
