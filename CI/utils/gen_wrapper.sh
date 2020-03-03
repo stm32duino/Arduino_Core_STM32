e0f7781ddd70e4554e050fe122bd26539d9bb346
Frederic Pillon <frederic.pillon@st.com>
2020-03-03 15:02:23 +0100

    Use shfmt to format the gen_wrapper.sh script
    Using shfmt
    https://github.com/mvdan/sh
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- CI/utils/gen_wrapper.sh
+++ CI/utils/gen_wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
-set -o nounset                              # Treat unset variables as an error
+set -o nounset # Treat unset variables as an error
 
 # Base path
 Core_path=../..
@@ -23,24 +23,24 @@ LLoutInc_path=$Core_path/cores/arduino/stm32/LL
 CMSIS_Startupfile=$Core_path/cores/arduino/stm32/stm32_def_build.h
 
 # Variables
-series=($(find $HALDrivers_path -maxdepth 1 -type d -name "STM32*" | sed -e "s#$HALDrivers_path/STM32##g" -e "s#xx_HAL_Driver##g"))
+series=($(find $HALDrivers_path -maxdepth 1 -type d -name "STM32*" | sed -e "s#$HALDrivers_path/STM32##g" -e "s#xx_HAL_Driver##g" | sort))
 
 all_LL_file=stm32yyxx_ll.h
 
 # Create the file
 print_C_header() {
-if [[ $1 = *"template"* ]]; then
-  echo "#if 0" > "$1"
-else
-  touch "$1"
-fi
+  if [[ $1 = *"template"* ]]; then
+    echo "#if 0" > "$1"
+  else
+    touch "$1"
+  fi
 }
 
 # Add some pragma to ll header files to avoid several warnings
 # which will be corrected along Cube update
 print_LL_header() {
-upper=$(echo "$1" | awk '{print toupper($1)}' | sed -e "s/\./_/g")
-echo "#ifndef _${upper}_
+  upper=$(echo "$1" | awk '{print toupper($1)}' | sed -e "s/\./_/g")
+  echo "#ifndef _${upper}_
 #define _${upper}_
 /* LL raised several warnings, ignore them */
 #pragma GCC diagnostic push
@@ -50,14 +50,14 @@ echo "#ifndef _${upper}_
 }
 
 print_CMSIS_Startup_header() {
-echo "#ifndef _STM32_DEF_BUILD_
+  echo "#ifndef _STM32_DEF_BUILD_
 #define _STM32_DEF_BUILD_
 
 #if !defined(CMSIS_STARTUP_FILE) && !defined(CUSTOM_STARTUP_FILE)" > $CMSIS_Startupfile
 }
 
 print_CMSIS_Startup_footer() {
-echo "#else
+  echo "#else
 #error UNKNOWN CHIP
 #endif
 #else
@@ -67,25 +67,24 @@ echo "#else
 }
 
 print_CMSIS_Startup_list() {
-# Handle first elements
-# File name
-local f
-local upper
-f=$(echo "${list[0]}" | awk -F/ '{print $NF}')
-upper=$(echo "$f" | awk -F'[_.]' '{print toupper($2)}' | tr X x)
-echo "#if defined($upper)
+  # Handle first elements
+  # File name
+  local f
+  local upper
+  f=$(echo "${list[0]}" | awk -F/ '{print $NF}')
+  upper=$(echo "$f" | awk -F'[_.]' '{print toupper($2)}' | tr X x)
+  echo "#if defined($upper)
 #define CMSIS_STARTUP_FILE \"$f\"" >> $CMSIS_Startupfile
 
-if [ ${#list[@]} -gt 1 ]; then
-	for fp in "${list[@]:1}"
-    do
-    # File name
-    f=$(echo "$fp" | awk -F/ '{print $NF}')
-		upper=$(echo "$f" | awk -F'[_.]' '{print toupper($2)}' | tr X x)
-		echo "#elif defined($upper)
+  if [ ${#list[@]} -gt 1 ]; then
+    for fp in "${list[@]:1}"; do
+      # File name
+      f=$(echo "$fp" | awk -F/ '{print $NF}')
+      upper=$(echo "$f" | awk -F'[_.]' '{print toupper($2)}' | tr X x)
+      echo "#elif defined($upper)
 #define CMSIS_STARTUP_FILE \"$f\"" >> $CMSIS_Startupfile
-	done
-fi
+    done
+  fi
 }
 
 # main
@@ -100,22 +99,20 @@ fi
 rm -f $HALoutSrc_path/* $LLoutSrc_path/* $LLoutInc_path/* $CMSIS_Startupfile
 
 # Search all files for each series
-for serie in "${series[@]}"
-do
+for serie in "${series[@]}"; do
   if [ -d $HALDrivers_path/STM32"${serie}"xx_HAL_Driver/Src ]; then
     lower=$(echo "$serie" | awk '{print tolower($0)}')
     echo -n "Generating for $serie..."
 
     # Generate stm32yyxx_[hal|ll]*.c file
     filelist=($(find $HALDrivers_path/STM32"${serie}"xx_HAL_Driver/Src -maxdepth 1 -name "stm32${lower}xx_*.c"))
-    for fp in "${filelist[@]}"
-    do
+    for fp in "${filelist[@]}"; do
       outp=$HALoutSrc_path
       # File name
       f=$(echo "$fp" | awk -F/ '{print $NF}')
       # Compute generic file name
       g="${f//$lower/yy}"
-      if [[  $f = *"_ll_"* ]]; then
+      if [[ $f = *"_ll_"* ]]; then
         outp=$LLoutSrc_path
       fi
       if [ ! -f $outp/"$g" ]; then
@@ -126,13 +123,12 @@ do
         echo "#ifdef STM32${serie}xx"
         echo "#include \"$f\""
         echo "#endif"
-       } >> $outp/"$g"
+      } >> $outp/"$g"
     done
 
     # Generate stm32yyxx_ll_*.h file
     filelist=($(find $HALDrivers_path/STM32"${serie}"xx_HAL_Driver/Inc -maxdepth 2 -name "stm32${lower}xx_ll_*.h"))
-    for fp in "${filelist[@]}"
-    do
+    for fp in "${filelist[@]}"; do
       # File name
       f=$(echo "$fp" | awk -F/ '{print $NF}')
       # Compute generic file name
@@ -145,10 +141,10 @@ do
       echo "#include \"$g\"" >> $LLoutInc_path/${all_LL_file}.tmp
       # Amend file name under serie switch
       {
-      echo "#ifdef STM32${serie}xx"
-      echo "#include \"$f\""
-      echo "#endif"
-       } >> $LLoutInc_path/"$g"
+        echo "#ifdef STM32${serie}xx"
+        echo "#include \"$f\""
+        echo "#endif"
+      } >> $LLoutInc_path/"$g"
     done
     echo "done"
   fi
@@ -164,15 +160,13 @@ rm -f $LLoutInc_path/${all_LL_file}.tmp
 
 # Search all template file to end "#if 0"
 filelist=($(find $HALoutSrc_path -maxdepth 1 -name "stm32*_template.c"))
-for fp in "${filelist[@]}"
-do
- echo "#endif /* 0 */" >> "$fp"
+for fp in "${filelist[@]}"; do
+  echo "#endif /* 0 */" >> "$fp"
 done
 
 # Search all LL header files to end guard
 filelist=($(find $LLoutInc_path -maxdepth 1 -name "stm32yyxx_ll*.h"))
-for fp in "${filelist[@]}"
-do
+for fp in "${filelist[@]}"; do
   upper=$(basename "$fp" | awk '{print toupper($1)}' | sed -e "s/\./_/g")
   echo "#pragma GCC diagnostic pop" >> "$fp"
   echo "#endif /* _${upper}_ */" >> "$fp"
@@ -181,12 +175,12 @@ done
 # CMSIS startup files
 list=($(find $CMSIS_Device_ST_path -name "startup_*.s" | grep gcc | sort -du))
 if [ ${#list[@]} -ne 0 ]; then
-	echo "Number of startup files: ${#list[@]}"
-	print_CMSIS_Startup_header
-	print_CMSIS_Startup_list
-	print_CMSIS_Startup_footer
+  echo "Number of startup files: ${#list[@]}"
+  print_CMSIS_Startup_header
+  print_CMSIS_Startup_list
+  print_CMSIS_Startup_footer
 else
-	echo "No startup files found!"
+  echo "No startup files found!"
 fi
 
 # CMSIS DSP C source files
@@ -199,8 +193,7 @@ else
   rm -fr "${CMSIS_DSP_outSrc_path:?}"/*/
 
   filelist=($(find $CMSIS_DSPSrc_path -name "*.c" | sed -e "s#$CMSIS_DSPSrc_path/##g" | sort -u))
-  for fp in "${filelist[@]}"
-  do
+  for fp in "${filelist[@]}"; do
     # Directory name
     d=$(echo "$fp" | awk -F/ '{print $(NF-1)}')
     if [ ! -d $CMSIS_DSP_outSrc_path/"$d" ]; then
