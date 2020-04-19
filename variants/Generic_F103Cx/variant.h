863f427f32d7f38aa70e888dfdf3fb8bd078710e
MCUdude <hansibull@gmail.com>
2020-04-19 19:56:39 +0200

    Add generic F103Cx targets

---
+++ variants/Generic_F103Cx/variant.h
@@ -0,0 +1,141 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2018, STMicroelectronics
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of STMicroelectronics nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************
+ */
+#ifndef _VARIANT_ARDUINO_STM32_
+#define _VARIANT_ARDUINO_STM32_
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+// * = F103C8-CB    | DIGITAL | ANALOG | USART      | TWI       | SPI        | SPECIAL   |
+//                  |---------|--------|------------|-----------|------------|-----------|
+#define PA0  A0  // | 0       | A0     |            |           |            |           |
+#define PA1  A1  // | 1       | A1     |            |           |            |           |
+#define PA2  A2  // | 2       | A2     | USART2_TX  |           |            |           |
+#define PA3  A3  // | 2       | A2     | USART2_RX  |           |            |           |
+#define PA4  A4  // | 4       | A4     |            |           | SPI1_SS    |           |
+#define PA5  A5  // | 5       | A5     |            |           | SPI1_SCK   |           |
+#define PA6  A6  // | 6       | A6     |            |           | SPI1_MISO  |           |
+#define PA7  A7  // | 7       | A7     |            |           | SPI1_MOSI  |           |
+#define PA8  8   // | 8       |        |            |           |            |           |
+#define PA9  9   // | 9       |        | USART1_TX  |           |            |           |
+#define PA10 10  // | 10      |        | USART1_RX  |           |            |           |
+#define PA11 11  // | 11      |        |            |           |            | USB_DN    |
+#define PA12 12  // | 12      |        |            |           |            | USB_DP    |
+#define PA13 13  // | 13      |        |            |           |            | SWD_SWDIO |
+#define PA14 14  // | 14      |        |            |           |            | SWD_SWCLK |
+#define PA15 15  // | 15      |        |            |           | SPI1_SS    |           |
+//                  |---------|--------|------------|-----------|------------|-----------|
+#define PB0  A8  // | 16      | A8     |            |           |            |           |
+#define PB1  A9  // | 17      | A9     |            |           |            |           |
+#define PB2  18  // | 18      |        |            |           |            | BOOT1     |
+#define PB3  19  // | 19      |        |            |           | SPI1_SCK   |           |
+#define PB4  20  // | 20      |        |            |           | SPI1_MISO  |           |
+#define PB5  21  // | 21      |        |            |           | SPI1_MOSI  |           |
+#define PB6  22  // | 22      |        | USART1_TX  | TWI1_SCL  |            |           |
+#define PB7  23  // | 23      |        | USART1_RX  | TWI1_SDA  |            |           |
+#define PB8  24  // | 24      |        |            | TWI1_SCL  |            |           |
+#define PB9  25  // | 25      |        |            | TWI1_SDA  |            |           |
+#define PB10 26  // | 26      |        | USART3_TX* | TWI2_SCL* |            |           |
+#define PB11 27  // | 27      |        | USART3_RX* | TWI2_SDA* |            |           |
+#define PB12 28  // | 28      |        |            |           | SPI2_SS*   |           |
+#define PB13 29  // | 29      |        |            |           | SPI2_SCK*  |           |
+#define PB14 30  // | 30      |        |            |           | SPI2_MISO* |           |
+#define PB15 31  // | 31      |        |            |           | SPI2_MOSI* |           |
+//                  |---------|--------|------------|-----------|------------|-----------|
+#define PC13 32  // | 32      |        |            |           |            |           |
+#define PC14 33  // | 33      |        |            |           |            | OSC32_IN  |
+#define PC15 34  // | 34      |        |            |           |            | OSC32_OUT |
+//                  |---------|--------|------------|-----------|------------|-----------|
+#define PD0  35  // | 35      |        |            |           |            | OSC_IN    |
+#define PD1  36  // | 36      |        |            |           |            | OSC_OUT   |
+//                  |---------|--------|------------|-----------|------------|-----------|
+
+
+// This must be a literal
+#define NUM_DIGITAL_PINS        37
+#define NUM_ANALOG_INPUTS       10
+
+// On-board LED pin number
+#ifndef LED_BUILTIN
+#define LED_BUILTIN             PC13
+#endif
+#define LED_GREEN               LED_BUILTIN
+
+// SPI Definitions
+#define PIN_SPI_SS              PA4
+#define PIN_SPI_MOSI            PA7
+#define PIN_SPI_MISO            PA6
+#define PIN_SPI_SCK             PA5
+
+// I2C Definitions
+#define PIN_WIRE_SDA            PB7
+#define PIN_WIRE_SCL            PB6
+
+// Timer Definitions
+// Use TIM6/TIM7 when possible as servo and tone don't need GPIO output pin
+#define TIMER_TONE              TIM3
+#define TIMER_SERVO             TIM2
+
+// UART Definitions
+#define SERIAL_UART_INSTANCE    1
+// Default pin used for 'Serial' instance
+// Mandatory for Firmata
+#define PIN_SERIAL_RX           PA10
+#define PIN_SERIAL_TX           PA9
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+  // These serial port names are intended to allow libraries and architecture-neutral
+  // sketches to automatically default to the correct port name for a particular type
+  // of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+  // the first hardware serial port whose RX/TX pins are not dedicated to another use.
+  //
+  // SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+  //
+  // SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+  //
+  // SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+  //
+  // SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+  //
+  // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+  //                            pins are NOT connected to anything by default.
+  #define SERIAL_PORT_MONITOR     Serial
+  #define SERIAL_PORT_HARDWARE    Serial1
+#endif
+
+#endif /* _VARIANT_ARDUINO_STM32_ */
