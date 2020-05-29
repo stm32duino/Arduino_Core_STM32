26f5806b035a206152e02a2ade8f9d05047d25df
Rudi Horn <dyn-git@rudi-horn.de>
2020-05-29 14:23:58 +0100

    initial commit of new cdc_msc and updated msc

---
+++ cores/arduino/stm32/usb/cdc_msc/usbd_cdc_msc.cpp
@@ -0,0 +1,508 @@
+/**
+  ******************************************************************************
+  * @file    usbd_template.c
+  * @author  MCD Application Team
+  * @brief   This file provides the HID core functions.
+  *
+  * @verbatim
+  *
+  *          ===================================================================
+  *                                COMPOSITE Class Description
+  *          ===================================================================
+  *
+  *
+  *
+  *
+  *
+  *
+  * @note     In HS mode and when the DMA is used, all variables and data structures
+  *           dealing with the DMA during the transaction process should be 32-bit aligned.
+  *
+  *
+  *  @endverbatim
+  *
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2015 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                      www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "usbd_cdc_msc.h"
+#include "usbd_ctlreq.h"
+
+
+/** @addtogroup STM32_USB_DEVICE_LIBRARY
+  * @{
+  */
+
+
+/** @defgroup USBD_COMPOSITE
+  * @brief usbd core module
+  * @{
+  */
+
+/** @defgroup USBD_COMPOSITE_Private_TypesDefinitions
+  * @{
+  */
+/**
+  * @}
+  */
+
+
+/** @defgroup USBD_COMPOSITE_Private_Defines
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+
+/** @defgroup USBD_COMPOSITE_Private_Macros
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+
+
+
+/** @defgroup USBD_COMPOSITE_Private_FunctionPrototypes
+  * @{
+  */
+
+
+static uint8_t  USBD_COMPOSITE_Init(USBD_HandleTypeDef *pdev,
+                                   uint8_t cfgidx);
+
+static uint8_t  USBD_COMPOSITE_DeInit(USBD_HandleTypeDef *pdev,
+                                     uint8_t cfgidx);
+
+static uint8_t  USBD_COMPOSITE_Setup(USBD_HandleTypeDef *pdev,
+                                    USBD_SetupReqTypedef *req);
+
+static uint8_t  *USBD_COMPOSITE_GetCfgDesc(uint16_t *length);
+
+static uint8_t  *USBD_COMPOSITE_GetDeviceQualifierDesc(uint16_t *length);
+
+static uint8_t  USBD_COMPOSITE_DataIn(USBD_HandleTypeDef *pdev, uint8_t epnum);
+
+static uint8_t  USBD_COMPOSITE_DataOut(USBD_HandleTypeDef *pdev, uint8_t epnum);
+
+static uint8_t  USBD_COMPOSITE_EP0_RxReady(USBD_HandleTypeDef *pdev);
+
+static uint8_t  USBD_COMPOSITE_EP0_TxReady(USBD_HandleTypeDef *pdev);
+
+static uint8_t  USBD_COMPOSITE_SOF(USBD_HandleTypeDef *pdev);
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_COMPOSITE_Private_Variables
+  * @{
+  */
+
+USBD_ClassTypeDef  USBD_COMPOSITE_ClassDriver =
+{
+  USBD_COMPOSITE_Init,
+  USBD_COMPOSITE_DeInit,
+  USBD_COMPOSITE_Setup,
+  nullptr,
+  USBD_COMPOSITE_EP0_RxReady,
+  USBD_COMPOSITE_DataIn,
+  USBD_COMPOSITE_DataOut,
+  USBD_COMPOSITE_SOF,
+  nullptr,
+  nullptr,
+  USBD_COMPOSITE_GetCfgDesc,
+  USBD_COMPOSITE_GetCfgDesc,
+  USBD_COMPOSITE_GetCfgDesc,
+  USBD_COMPOSITE_GetDeviceQualifierDesc,
+};
+
+
+#if defined ( __ICCARM__ ) /*!< IAR Compiler */
+#pragma data_alignment=4
+#endif
+/* USB COMPOSITE device Configuration Descriptor */
+static uint8_t USBD_COMPOSITE_CfgDesc[USB_CDC_MSC_CONFIG_DESC_SIZ] =
+{
+  0x09, /* bLength: Configuation Descriptor size */
+  USB_DESC_TYPE_OTHER_SPEED_CONFIGURATION, /* bDescriptorType: Configuration */
+  USB_CDC_MSC_CONFIG_DESC_SIZ,
+  /* wTotalLength: Bytes returned */
+  0x00,
+  0x03,         /*bNumInterfaces: 3 interface*/
+  0x01,         /*bConfigurationValue: Configuration value*/
+  0x02,         /*iConfiguration: Index of string descriptor describing the configuration*/
+  0xC0,         /*bmAttributes: bus powered and Supports Remote Wakeup */
+  0x32,         /*MaxPower 100 mA: this current is used for detecting Vbus*/
+  /* 09 */
+
+  /*---------------------------------------------------------------------------*/
+
+  /*Interface Descriptor */
+  0x09,   /* bLength: Interface Descriptor size */
+  USB_DESC_TYPE_INTERFACE,  /* bDescriptorType: Interface */
+  /* Interface descriptor type */
+  0x00,   /* bInterfaceNumber: Number of Interface */
+  0x00,   /* bAlternateSetting: Alternate setting */
+  0x01,   /* bNumEndpoints: One endpoints used */
+  0x02,   /* bInterfaceClass: Communication Interface Class */
+  0x02,   /* bInterfaceSubClass: Abstract Control Model */
+  0x01,   /* bInterfaceProtocol: Common AT commands */
+  0x00,   /* iInterface: */
+
+  /*Header Functional Descriptor*/
+  0x05,   /* bLength: Endpoint Descriptor size */
+  0x24,   /* bDescriptorType: CS_INTERFACE */
+  0x00,   /* bDescriptorSubtype: Header Func Desc */
+  0x10,   /* bcdCDC: spec release number */
+  0x01,
+
+  /*Call Management Functional Descriptor*/
+  0x05,   /* bFunctionLength */
+  0x24,   /* bDescriptorType: CS_INTERFACE */
+  0x01,   /* bDescriptorSubtype: Call Management Func Desc */
+  0x00,   /* bmCapabilities: D0+D1 */
+  0x01,   /* bDataInterface: 1 */
+
+  /*ACM Functional Descriptor*/
+  0x04,   /* bFunctionLength */
+  0x24,   /* bDescriptorType: CS_INTERFACE */
+  0x02,   /* bDescriptorSubtype: Abstract Control Management desc */
+  0x02,   /* bmCapabilities */
+
+  /*Union Functional Descriptor*/
+  0x05,   /* bFunctionLength */
+  0x24,   /* bDescriptorType: CS_INTERFACE */
+  0x06,   /* bDescriptorSubtype: Union func desc */
+  0x00,   /* bMasterInterface: Communication class interface */
+  0x01,   /* bSlaveInterface0: Data Class Interface */
+
+  /*Endpoint 2 Descriptor*/
+  0x07,                           /* bLength: Endpoint Descriptor size */
+  USB_DESC_TYPE_ENDPOINT,   /* bDescriptorType: Endpoint */
+  CDC_CMD_EP,                     /* bEndpointAddress */
+  0x03,                           /* bmAttributes: Interrupt */
+  LOBYTE(CDC_CMD_PACKET_SIZE),     /* wMaxPacketSize: */
+  HIBYTE(CDC_CMD_PACKET_SIZE),
+  CDC_FS_BINTERVAL,                           /* bInterval: */
+  /*---------------------------------------------------------------------------*/
+
+  /*Data class interface descriptor*/
+  0x09,   /* bLength: Endpoint Descriptor size */
+  USB_DESC_TYPE_INTERFACE,  /* bDescriptorType: */
+  0x01,   /* bInterfaceNumber: Number of Interface */
+  0x00,   /* bAlternateSetting: Alternate setting */
+  0x02,   /* bNumEndpoints: Two endpoints used */
+  0x0A,   /* bInterfaceClass: CDC */
+  0x00,   /* bInterfaceSubClass: */
+  0x00,   /* bInterfaceProtocol: */
+  0x00,   /* iInterface: */
+
+  /*Endpoint OUT Descriptor*/
+  0x07,   /* bLength: Endpoint Descriptor size */
+  USB_DESC_TYPE_ENDPOINT,      /* bDescriptorType: Endpoint */
+  CDC_OUT_EP,                        /* bEndpointAddress */
+  0x02,                              /* bmAttributes: Bulk */
+  LOBYTE(CDC_DATA_FS_MAX_PACKET_SIZE),  /* wMaxPacketSize: */
+  HIBYTE(CDC_DATA_FS_MAX_PACKET_SIZE),
+  0x00,                              /* bInterval: ignore for Bulk transfer */
+
+  /*Endpoint IN Descriptor*/
+  0x07,   /* bLength: Endpoint Descriptor size */
+  USB_DESC_TYPE_ENDPOINT,      /* bDescriptorType: Endpoint */
+  CDC_IN_EP,                         /* bEndpointAddress */
+  0x02,                              /* bmAttributes: Bulk */
+  LOBYTE(CDC_DATA_FS_MAX_PACKET_SIZE),  /* wMaxPacketSize: */
+  HIBYTE(CDC_DATA_FS_MAX_PACKET_SIZE),
+  0x00,
+
+
+  /********************  Mass Storage interface ********************/
+  0x09,                                            /* bLength: Interface Descriptor size */
+  0x04,                                            /* bDescriptorType: */
+  0x02,                                            /* bInterfaceNumber: Number of Interface */
+  0x00,                                            /* bAlternateSetting: Alternate setting */
+  0x02,                                            /* bNumEndpoints */
+  0x08,                                            /* bInterfaceClass: MSC Class */
+  0x06,                                            /* bInterfaceSubClass : SCSI transparent */
+  0x50,                                            /* nInterfaceProtocol */
+  0x05,                                            /* iInterface: */
+
+  /********************  Mass Storage Endpoints ********************/
+  0x07,                                            /* Endpoint descriptor length = 7 */
+  0x05,                                            /* Endpoint descriptor type */
+  MSC_EPIN_ADDR,                                   /* Endpoint address (IN, address 1) */
+  0x02,                                            /* Bulk endpoint type */
+  LOBYTE(MSC_MAX_HS_PACKET),
+  HIBYTE(MSC_MAX_HS_PACKET),
+  0x00,                                            /* Polling interval in milliseconds */
+
+  0x07,                                            /* Endpoint descriptor length = 7 */
+  0x05,                                            /* Endpoint descriptor type */
+  MSC_EPOUT_ADDR,                                  /* Endpoint address (OUT, address 1) */
+  0x02,                                            /* Bulk endpoint type */
+  LOBYTE(MSC_MAX_HS_PACKET),
+  HIBYTE(MSC_MAX_HS_PACKET),
+  0x00                                             /* Polling interval in milliseconds */
+};
+
+#if defined ( __ICCARM__ ) /*!< IAR Compiler */
+#pragma data_alignment=4
+#endif
+/* USB Standard Device Descriptor */
+static uint8_t USBD_COMPOSITE_DeviceQualifierDesc[USB_LEN_DEV_QUALIFIER_DESC] =
+{
+  USB_LEN_DEV_QUALIFIER_DESC,
+  USB_DESC_TYPE_DEVICE_QUALIFIER,
+  0x00,
+  0x02,
+  0x00,
+  0x00,
+  0x00,
+  0x40,
+  0x01,
+  0x00,
+};
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_COMPOSITE_Private_Functions
+  * @{
+  */
+
+/**
+  * @brief  USBD_COMPOSITE_Init
+  *         Initialize the COMPOSITE interface
+  * @param  pdev: device instance
+  * @param  cfgidx: Configuration index
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_Init(USBD_HandleTypeDef *pdev,
+                                   uint8_t cfgidx)
+{
+  USBD_MSC.Init(pdev, cfgidx);
+
+  USBD_CDC.Init(pdev, cfgidx);
+
+  return (uint8_t)USBD_OK;
+}
+
+/**
+  * @brief  USBD_COMPOSITE_Init
+  *         DeInitialize the COMPOSITE layer
+  * @param  pdev: device instance
+  * @param  cfgidx: Configuration index
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_DeInit(USBD_HandleTypeDef *pdev,
+                                     uint8_t cfgidx)
+{
+  USBD_MSC.DeInit(pdev, cfgidx);
+
+  USBD_CDC.DeInit(pdev, cfgidx);
+
+  return USBD_OK;
+}
+
+/**
+  * @brief  USBD_COMPOSITE_Setup
+  *         Handle the COMPOSITE specific requests
+  * @param  pdev: instance
+  * @param  req: usb requests
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_Setup(USBD_HandleTypeDef *pdev,
+                                    USBD_SetupReqTypedef *req)
+{
+  switch (req->bmRequest & USB_REQ_RECIPIENT_MASK)
+  {
+    case USB_REQ_RECIPIENT_INTERFACE:
+      switch(req->wIndex) {
+        case CDC_ACM_INTERFACE:
+        case CDC_COM_INTERFACE:
+          return USBD_CDC.Setup(pdev, req);
+          break;
+
+        case MSC_INTERFACE:
+          return USBD_MSC.Setup(pdev, req);
+          break;
+      
+        // invalid interface
+        default:
+          return USBD_FAIL;
+      }
+    break;
+
+    case USB_REQ_RECIPIENT_ENDPOINT:
+      switch (req->wIndex) {
+        case CDC_IN_EP:
+        case CDC_OUT_EP:
+        case CDC_CMD_EP:
+          return USBD_CDC.Setup(pdev, req);
+
+        case MSC_IN_EP:
+        case MSC_OUT_EP:
+          return USBD_MSC.Setup(pdev, req);
+
+        // invalid endpoint
+        default:
+          return USBD_FAIL;
+      }
+    break;
+
+    default:
+      break;
+  }
+
+  return USBD_OK;
+}
+
+
+/**
+  * @brief  USBD_COMPOSITE_GetCfgDesc
+  *         return configuration descriptor
+  * @param  length : pointer data length
+  * @retval pointer to descriptor buffer
+  */
+static uint8_t  *USBD_COMPOSITE_GetCfgDesc(uint16_t *length)
+{
+  *length = sizeof(USBD_COMPOSITE_CfgDesc);
+  return USBD_COMPOSITE_CfgDesc;
+}
+
+/**
+* @brief  DeviceQualifierDescriptor
+*         return Device Qualifier descriptor
+* @param  length : pointer data length
+* @retval pointer to descriptor buffer
+*/
+uint8_t  *USBD_COMPOSITE_DeviceQualifierDescriptor(uint16_t *length)
+{
+  *length = sizeof(USBD_COMPOSITE_DeviceQualifierDesc);
+  return USBD_COMPOSITE_DeviceQualifierDesc;
+}
+
+
+/**
+  * @brief  USBD_COMPOSITE_DataIn
+  *         handle data IN Stage
+  * @param  pdev: device instance
+  * @param  epnum: endpoint index
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_DataIn(USBD_HandleTypeDef *pdev,
+                                     uint8_t epnum)
+{
+  switch (epnum)
+  {
+    case CDC_IN_EP:
+    case CDC_CMD_EP:
+    case CDC_OUT_EP:
+      return USBD_CDC.DataIn(pdev, epnum);
+
+    case MSC_IN_EP:
+    case MSC_OUT_EP:
+      return USBD_MSC.DataIn(pdev, epnum);
+
+    // invalid endpoint
+    default:
+      return USBD_FAIL;
+  }
+}
+
+/**
+  * @brief  USBD_COMPOSITE_EP0_RxReady
+  *         handle EP0 Rx Ready event
+  * @param  pdev: device instance
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_EP0_RxReady(USBD_HandleTypeDef *pdev)
+{
+  // only needed by CDC
+  return USBD_CDC.EP0_RxReady(pdev);
+}
+
+/**
+  * @brief  USBD_COMPOSITE_SOF
+  *         handle SOF event
+  * @param  pdev: device instance
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_SOF(USBD_HandleTypeDef *pdev)
+{
+  // only needed for CDC
+  return USBD_CDC.SOF(pdev);
+}
+
+/**
+  * @brief  USBD_COMPOSITE_DataOut
+  *         handle data OUT Stage
+  * @param  pdev: device instance
+  * @param  epnum: endpoint index
+  * @retval status
+  */
+static uint8_t  USBD_COMPOSITE_DataOut(USBD_HandleTypeDef *pdev,
+                                      uint8_t epnum)
+{
+  switch (epnum)
+  {
+    case CDC_IN_EP:
+    case CDC_CMD_EP:
+    case CDC_OUT_EP:
+      return USBD_CDC.DataOut(pdev, epnum);
+
+    case MSC_IN_EP:
+    case MSC_OUT_EP:
+      return USBD_MSC.DataOut(pdev, epnum);
+
+    // invalid endpoint
+    default:
+      return USBD_FAIL;
+  }
+}
+
+/**
+* @brief  DeviceQualifierDescriptor
+*         return Device Qualifier descriptor
+* @param  length : pointer data length
+* @retval pointer to descriptor buffer
+*/
+uint8_t  *USBD_COMPOSITE_GetDeviceQualifierDesc(uint16_t *length)
+{
+  *length = sizeof(USBD_COMPOSITE_DeviceQualifierDesc);
+  return USBD_COMPOSITE_DeviceQualifierDesc;
+}
+
+/**
+  * @}
+  */
+
+
+/**
+  * @}
+  */
+
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
