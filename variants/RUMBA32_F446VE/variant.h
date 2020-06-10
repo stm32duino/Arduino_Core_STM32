f13ec1e671b8ad9a75811f9c2ff5462c3442730f
Chris <chris@chrisbarrbuilds.com>
2020-06-10 14:12:58 +0930

    Allow overriding timer definitions
    Allow TIMER_TONE, TIMER_SERVO and TIMER_SERIAL to be overridden using built_opt.h or similar.

--- variants/RUMBA32_F446VE/variant.h
+++ variants/RUMBA32_F446VE/variant.h
@@ -133,9 +133,17 @@ extern "C" {
 
 // Timer Definitions
 // Use TIM6/TIM7 when possible as servo and tone don't need GPIO output pin
+#ifndef TIMER_TONE
 #define TIMER_TONE              TIM6
+#endif
+
+#ifndef TIMER_SERVO
 #define TIMER_SERVO             TIM7
+#endif
+
+#ifndef TIMER_SERIAL
 #define TIMER_SERIAL            TIM9
+#endif
 
 // UART Definitions
 #define SERIAL_UART_INSTANCE    1 // Connected to EXP3 header
