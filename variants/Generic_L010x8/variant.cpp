264a09cd8171d246b7df4dc1673dbc631b82443a
Morten Kals <morten@kals.no>
2020-07-18 10:32:25 +0200

    added initial configuration for L010R8

---
+++ variants/Generic_L010x8/variant.cpp
@@ -0,0 +1,175 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2019, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+
+#include "pins_arduino.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Digital PinName array
+// This array allows to wrap Arduino pin number(Dx or x)
+// to STM32 PinName (PX_n)
+const PinName digitalPin[] = {
+  //PX_n, //Dx
+  PA_0, //  D0  / A0
+  PA_1, //  D1  / A1
+  PA_2, //  D2  / A2
+  PA_3, //  D3  / A3
+  PA_4, //  D4  / A4
+  PA_5, //  D5  / A5
+  PA_6, //  D6  / A6
+  PA_7, //  D7  / A7
+  PA_8, //  D8
+  PA_9, //  D9
+  PA_10, // D10
+  PA_11, // D11
+  PA_12, // D12
+  PA_13, // D13
+  PA_14, // D14
+  PA_15, // D15
+  
+  PB_0, //  D16  / A8
+  PB_1, //  D17  / A9
+  PB_2, //  D18
+  PB_3, //  D19
+  PB_4, //  D20
+  PB_5, //  D21
+  PB_6, //  D22
+  PB_7, //  D23
+  PB_8, //  D24
+  PB_9, //  D25
+  PB_10, // D26
+  PB_11, // D27
+  PB_12, // D28
+  PB_13, // D29
+  PB_14, // D30
+  PB_15, // D31
+
+  PC_0, //  D32  / A10
+  PC_1, //  D33  / A11
+  PC_2, //  D34  / A12
+  PC_3, //  D35  / A13
+  PC_4, //  D36  / A14
+  PC_5, //  D37  / A15
+  PC_6, //  D38
+  PC_7, //  D39
+  PC_8, //  D40
+  PC_9, //  D41
+  PC_10, // D42
+  PC_11, // D43
+  PC_12, // D44
+  PC_13, // D45
+  
+  PD_2, //  D46
+};
+
+// If analog pins are not contiguous in the digitalPin array:
+// Add the analogInputPin array without defining NUM_ANALOG_FIRST
+// Analog (Ax) pin number array
+// where x is the index to retrieve the digital pin number
+const uint32_t analogInputPin[] = {
+  //PXn, //Ax = Dx
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  16,
+  17, 
+  32, 
+  33, 
+  34, 
+  35, 
+  36, 
+  37,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+// ----------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+  * @brief  System Clock Configuration
+  * @param  None
+  * @retval None
+  */
+WEAK void SystemClock_Config(void)
+{
+  // Here copy the desired System Clock Configuration
+  // It could be generated thanks STM32CubeMX after code generation for Toolchain/IDE: 'SW4STM32',
+  // available in src/main.c
+  // or
+  // copied from a STM32CubeYY project examples
+  // where 'YY' could be F0, F1, F2, F3, F4, F7, G0, G4, H7, L0, L1, L4, MP1, WB
+
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_8;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_RTC
+                              |RCC_PERIPHCLK_LPTIM1;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+  PeriphClkInit.LptimClockSelection = RCC_LPTIM1CLKSOURCE_PCLK;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Enables the Clock Security System 
+  */
+  HAL_RCC_EnableCSS();
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ variants/Generic_L010x8/variant.h
@@ -0,0 +1,269 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2019, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+
+#ifndef _VARIANT_ARDUINO_STM32_
+#define _VARIANT_ARDUINO_STM32_
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/*----------------------------------------------------------------------------
+ *        Pins
+ *----------------------------------------------------------------------------*/
+
+// Define pin names to match digital pin number --> Dx
+// It could be used with preprocessor tests (e.g. #if PXn == 3)
+// so an enum will not work.
+// !!!
+// !!! Copy the digitalPin[] array from the variant.cpp
+// !!! and remove all '_': PX_n --> PXn
+// !!! For NC, comment the line to warn x pin number is NC
+// !!! // x is NC
+// !!! For duplicated pin name, comment the line to warn x pin number
+// !!! is PXn which is already defined with y pin number
+// !!! // x is PXn (y)
+// !!! Ex:
+// !!! ...
+// !!! #define PA4  20 // A14 <-- if NUM_ANALOG_FIRST not defined
+// !!! or
+// !!! #define PA4  A14 // 20 <-- if NUM_ANALOG_FIRST defined
+// !!! #define PB4  21
+// !!! #define PB5  22
+// !!! #define PB3  23
+// !!! // 24 is PA4 (20)
+// !!! // 25 is PB4 (21)
+// !!! #define PA2  26 // A15 <-- if NUM_ANALOG_FIRST not defined
+// !!! or
+// !!! #define PA2  A15 // 26 <-- if NUM_ANALOG_FIRST defined
+
+#define PA0   0 // A0
+#define PA1   1 // A1
+#define PA2   2 // A2
+#define PA3   3 // A3
+#define PA4   4 // A4
+#define PA5   5 // A5
+#define PA6   6 // A6
+#define PA7   7 // A7
+#define PA8   8
+#define PA9   9
+#define PA10  10
+#define PA11  11
+#define PA12  12
+#define PA13  13
+#define PA14  14
+#define PA15  15
+
+#define PB0   16 // A8
+#define PB1   17 // A9
+#define PB2   18
+#define PB3   19
+#define PB4   20
+#define PB5   21
+#define PB6   22
+#define PB7   23
+#define PB8   24
+#define PB9   25
+#define PB10  26
+#define PB11  27
+#define PB12  28
+#define PB13  29
+#define PB14  30
+#define PB15  31
+
+#define PC0   32 // A10
+#define PC1   33 // A11
+#define PC2   34 // A12
+#define PC3   35 // A13
+#define PC4   36 // A14
+#define PC5   37 // A15
+#define PC6   38 
+#define PC7   39 
+#define PC8   40 
+#define PC9   41
+#define PC10  42
+#define PC11  43
+#define PC12  44
+#define PC13  45
+
+#define PD2   46
+
+
+// This must be a literal
+// It is used with preprocessor tests (e.g. #if NUM_DIGITAL_PINS > 3)
+// so an enum will not work.
+#define NUM_DIGITAL_PINS        47
+
+// Allow to define Arduino style alias for analog input pin number --> Ax
+// If no analog pin required then NUM_ANALOG_INPUTS could not be defined
+// or set to `0`
+// All pins are digital, analog inputs are a subset of digital pins.
+// This must be a literal
+// It is used with preprocessor tests (e.g. #if NUM_ANALOG_INPUTS > 3)
+// so an enum will not work.
+// !!!
+// !!! It must be aligned with the number of analog PinName
+// !!! defined in digitalPin[] array in variant.cpp
+// !!!
+#define NUM_ANALOG_INPUTS       16
+
+// They are 2 possibles way to define analog pins:
+//-------------------------------------------------------------------------------------------
+//   - If they are contiguous in the digitalPin array:
+//     Simply defined `NUM_ANALOG_FIRST` and all pins Ax will be automatically defined.
+//     It define the digital pin number of the first analog input  (i.e. which digital pin is A0)
+//     First analog pin value (A0) must be greater than or equal to NUM_ANALOG_INPUTS
+//     This must be a literal with a value less than or equal to MAX_ANALOG_INPUTS
+//     defined in pin_arduino.h
+//#define NUM_ANALOG_FIRST        0
+//------------------------------------OR------------------------------------------------------
+//   - If they are not contiguous in the digitalPin array:
+//     Add an analogInputPin array in the variant.cpp without defining NUM_ANALOG_FIRST
+//     In that case the defined PYn for analog pin have to define the Ax definition instead of
+//     index in digitalPin[] array:
+//     #define PA4  A14
+//-------------------------------------------------------------------------------------------
+
+// Below ADC and PWM definitions already done in the core
+// Could be redefined here if needed
+// ADC resolution is 10 bits
+//#define ADC_RESOLUTION          10
+
+// PWM resolution
+#define PWM_RESOLUTION          8
+#define PWM_FREQUENCY           1000
+#define PWM_MAX_DUTY_CYCLE      255
+#define HAL_TIM_MODULE_ENABLED
+
+// On-board LED pin number
+#define LED_BUILTIN             PC2
+#define LED_GREEN               LED_BUILTIN
+
+// On-board user button
+#define USER_BTN                PB2
+
+// Below SPI and I2C definitions already done in the core
+// Could be redefined here if differs from the default one
+// SPI Definitions
+#define PIN_SPI_SS              PB8 // Default for Arduino connector compatibility
+#define PIN_SPI_MOSI            PB5 // Default for Arduino connector compatibility
+#define PIN_SPI_MISO            PB4 // Default for Arduino connector compatibility
+#define PIN_SPI_SCK             PB3 // Default for Arduino connector compatibility
+
+// I2C Definitions
+//#define PIN_WIRE_SDA            14 // Default for Arduino connector compatibility
+//#define PIN_WIRE_SCL            15 // Default for Arduino connector compatibility
+
+// I2C timing definitions (optional), avoid time spent to compute if defined
+// * I2C_TIMING_SM for Standard Mode (100kHz)
+// * I2C_TIMING_FM for Fast Mode (400kHz)
+// * I2C_TIMING_FMP for Fast Mode Plus (1000kHz)
+//#define I2C_TIMING_SM           0x00000000
+//#define I2C_TIMING_FM           0x00000000
+//#define I2C_TIMING_FMP          0x00000000
+
+// Timer Definitions (optional)
+// Use TIM6/TIM7 when possible as servo and tone don't need GPIO output pin
+#define TIMER_TONE              TIM2
+#define TIMER_SERVO             TIM2
+
+// UART Definitions
+// Define here Serial instance number to map on Serial generic name
+#define SERIAL_UART_INSTANCE    2 //ex: 2 for Serial2 (USART2)
+
+// DEBUG_UART could be redefined to print on another instance than 'Serial'
+//#define DEBUG_UART              ((USART_TypeDef *) U(S)ARTX) // ex: USART3
+// DEBUG_UART baudrate, default: 9600 if not defined
+//#define DEBUG_UART_BAUDRATE     x
+// DEBUG_UART Tx pin name, default: the first one found in PinMap_UART_TX for DEBUG_UART
+//#define DEBUG_PINNAME_TX        PX_n // PinName used for TX
+
+// Default pin used for 'Serial' instance (ex: ST-Link)
+// Mandatory for Firmata
+#define PIN_SERIAL_RX           PA3
+#define PIN_SERIAL_TX           PA2
+
+// Optional PIN_SERIALn_RX and PIN_SERIALn_TX where 'n' is the U(S)ART number
+// Used when user instanciate a hardware Serial using its peripheral name.
+// Example: HardwareSerial mySerial(USART3);
+// will use PIN_SERIAL3_RX and PIN_SERIAL3_TX if defined.
+//#define PIN_SERIALn_RX          x // For U(S)ARTn RX
+//#define PIN_SERIALn_TX          x // For U(S)ARTn TX
+//#define PIN_SERIALLP1_RX        x // For LPUART1 RX
+//#define PIN_SERIALLP1_TX        x // For LPUART1 TX
+
+// SD card slot Definitions
+// SD detect signal can be defined if required
+//#define SD_DETECT_PIN           x
+// SD Read/Write timeout, default value defined in STM32SD library
+//#define SD_DATATIMEOUT          x
+
+// USB Vbus sensing. Require to have Vbus pin connected to Vbus signal.
+// Warning, pin is different depending on FullSpeed or High Speed mode used
+// See AN4879 https://www.st.com/content/st_com/en/search.html#q=AN4879-t=resources-page=1
+//#define USBD_VBUS_DETECTION_ENABLE
+
+// If the board has external USB pullup (on DP/DM depending on speed)
+// that can be controlled using a GPIO pin, define these:
+//  - If the the pullup is disabled (USB detached) by default, define
+//    USBD_ATTACH_PIN to the pin that, when written to
+//    USBD_ATTACH_LEVEL, attaches the pullup.
+//  - If the the pullup is enabled (attached) by default, define
+//    USBD_DETACH_PIN to the pin that, when written to
+//    USBD_DETACH_LEVEL, detaches the pullup.
+//#define USBD_ATTACH_PIN x
+//#define USBD_ATTACH_LEVEL LOW
+//#define USBD_DETACH_PIN x
+//#define USBD_DETACH_LEVEL LOW
+//
+// This indicates that there is an external and fixed 1.5k pullup
+// on the D+ line. This define is not normally needed, since a
+// fixed pullup is assumed by default. It is only required when
+// the USB peripheral has an internal pullup *and* an external
+// fixed pullup is present (which is actually a hardware bug, since just
+// the internal pullup is sufficient and having two pullups violates the
+// USB specification). In this case, defining this forces
+// the "write D+ LOW"-trick to be used. In the future, it might also
+// disable the internal pullups, but this is not currently implemented.
+// #define USBD_FIXED_PULLUP
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+  // These serial port names are intended to allow libraries and architecture-neutral
+  // sketches to automatically default to the correct port name for a particular type
+  // of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+  // the first hardware serial port whose RX/TX pins are not dedicated to another use.
+  //
+  // SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+  //
+  // SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+  //
+  // SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+  //
+  // SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+  //
+  // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+  //                            pins are NOT connected to anything by default.
+  #define SERIAL_PORT_MONITOR     Serial
+  #define SERIAL_PORT_HARDWARE    Serial
+
+#endif
+
+#endif /* _VARIANT_ARDUINO_STM32_ */
