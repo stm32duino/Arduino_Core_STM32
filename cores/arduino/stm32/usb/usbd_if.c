63c49b0adcec04bcb803f5ad70650c38a5674f60
Matthijs Kooijman <matthijs@stdin.nl>
2020-09-03 17:23:42 +0200

    HACK: Do not use USB_DevDisconnect in early startup
    This function calls HAL_Delay, which relies on the systick timer to be
    running, so this results in an infinite loop. This should probably be
    fixed in HAL_Delay, but for now just remove the calls to
    USB_DevDisconnect and USB_DevConnect and replace it with the bit
    twiddles needed for some chip families (e.g. the F4), breaking
    compilation and/or functionality for others.

--- cores/arduino/stm32/usb/usbd_if.c
+++ cores/arduino/stm32/usb/usbd_if.c
@@ -194,9 +194,12 @@ WEAK void USBD_reenumerate(void)
   digitalWriteFast(USBD_PULLUP_CONTROL_PINNAME, USBD_ATTACH_LEVEL);
 #endif /* defined(USBD_PULLUP_CONTROL_FLOATING) */
 #elif defined(USBD_HAVE_INTERNAL_PULLUPS)
-  USB_DevDisconnect(USBD_USB_INSTANCE);
+  uint32_t USBx_BASE = (uint32_t)USBD_USB_INSTANCE;
+  USBx_DEVICE->DCTL |= USB_OTG_DCTL_SDIS;
+  //USB_DevDisconnect(USBD_USB_INSTANCE);
   USBD_early_startup_delay_us(USBD_ENUM_DELAY * 1000);
-  USB_DevConnect(USBD_USB_INSTANCE);
+  //USB_DevConnect(USBD_USB_INSTANCE);
+  USBx_DEVICE->DCTL &= ~USB_OTG_DCTL_SDIS;
 #else
 #warning "No USB attach/detach method, USB might not be reliable through system resets"
 #endif
