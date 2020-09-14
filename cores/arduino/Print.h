7dfd03b7157b558f53ba0244b2fd8774cc1c2135
Frederic Pillon <frederic.pillon@st.com>
2020-09-14 18:53:41 +0200

    Added Print.availableForWrite()
    Fix #1170
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cores/arduino/Print.h
+++ cores/arduino/Print.h
@@ -68,6 +68,13 @@ class Print {
       return write((const uint8_t *)buffer, size);
     }
 
+    // default to zero, meaning "a single write may block"
+    // should be overriden by subclasses with buffering
+    virtual int availableForWrite()
+    {
+      return 0;
+    }
+
     size_t print(const __FlashStringHelper *);
     size_t print(const String &);
     size_t print(const char[]);
