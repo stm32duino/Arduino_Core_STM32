44f3ef7acd92c334ea2b3f9511c45e4b76027f35
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2021-06-03 07:45:33 +0200

    build options in source comments

--- system/extras/prebuild.sh
+++ system/extras/prebuild.sh
@@ -2,6 +2,7 @@
 
 BUILD_PATH="$1"
 BUILD_SOURCE_PATH="$2"
+BUILD_PROJECT_NAME="$3"
 
 # Create sketch dir if not exists
 if [ ! -f "$BUILD_PATH/sketch" ]; then
@@ -11,6 +12,11 @@ fi
 # Create empty build.opt.h if not exists in the original sketch dir
 if [ ! -f "$BUILD_SOURCE_PATH/build_opt.h" ]; then
   touch "$BUILD_PATH/sketch/build_opt.h"
+fi
+
+# add build options in source comments
+if [ -f "$BUILD_SOURCE_PATH/$BUILD_PROJECT_NAME" ]; then
+  grep '\/\/ build-opt' "$BUILD_SOURCE_PATH/$BUILD_PROJECT_NAME" | sed -e 's/\/\/ build-opt//' >> "$BUILD_PATH/sketch/build_opt.h"
 fi
 
 # Force include of SrcWrapper library
