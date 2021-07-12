7d01ee3dd83423f176f057f49aa26a474c590ed8
Frederic Pillon <frederic.pillon@st.com>
2021-07-12 11:17:26 +0200

    system: U5: update STM32U5xx system
    Remove duplicate clock definition.
    Allow VECT_TAB_OFFSET redefinition.
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- system/STM32U5xx/system_stm32u5xx.c
+++ system/STM32U5xx/system_stm32u5xx.c
@@ -119,24 +119,15 @@
   * @{
   */
 
-#if !defined  (HSE_VALUE)
-  #define HSE_VALUE    16000000U /*!< Value of the External oscillator in Hz */
-#endif /* HSE_VALUE */
-
-#if !defined  (MSI_VALUE)
-  #define MSI_VALUE    4000000U  /*!< Value of the Internal oscillator in Hz*/
-#endif /* MSI_VALUE */
-
-#if !defined  (HSI_VALUE)
-  #define HSI_VALUE    16000000U /*!< Value of the Internal oscillator in Hz*/
-#endif /* HSI_VALUE */
 
 /************************* Miscellaneous Configuration ************************/
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
 /* #define VECT_TAB_SRAM */
+#ifndef VECT_TAB_OFFSET
 #define VECT_TAB_OFFSET  0x00000000UL /*!< Vector Table base offset field.
                                    This value must be a multiple of 0x200. */
+#endif
 /******************************************************************************/
 
 /**
