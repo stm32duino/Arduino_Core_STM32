eb9b4ed3e6a4c28888c51d8ba93448c7c5bebd51
Frederic Pillon <frederic.pillon@st.com>
2021-08-11 10:52:40 +0200

    variants: L0: update generated files to STM32_open_pin_data v6.0.30
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32L0xx/L010RBT/PeripheralPins.c
+++ variants/STM32L0xx/L010RBT/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32L010RBTx.xml
- * CubeMX DB release 6.0.21
+ * CubeMX DB release 6.0.30
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
--- variants/STM32L0xx/L051T(6-8)Y/PeripheralPins.c
+++ variants/STM32L0xx/L051T(6-8)Y/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32L051T(6-8)Yx.xml
- * CubeMX DB release 6.0.21
+ * CubeMX DB release 6.0.30
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
