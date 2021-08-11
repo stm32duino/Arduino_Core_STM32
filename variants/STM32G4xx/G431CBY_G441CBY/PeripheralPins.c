d7979f54e57e713725cafbb4db100130dfdf601f
Frederic Pillon <frederic.pillon@st.com>
2021-08-11 09:53:03 +0200

    variants: G4: update generated files to STM32_open_pin_data v6.0.30
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32G4xx/G431CBY_G441CBY/PeripheralPins.c
+++ variants/STM32G4xx/G431CBY_G441CBY/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32G431CBYx.xml, STM32G441CBYx.xml
- * CubeMX DB release 6.0.21
+ * CubeMX DB release 6.0.30
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
