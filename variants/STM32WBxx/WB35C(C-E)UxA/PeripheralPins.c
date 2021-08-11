7253dddd430e862349a5d6a36e6fd1f453eb233c
Frederic Pillon <frederic.pillon@st.com>
2021-08-11 11:13:18 +0200

    variants: WB: update generated files to STM32_open_pin_data v6.0.30
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32WBxx/WB35C(C-E)UxA/PeripheralPins.c
+++ variants/STM32WBxx/WB35C(C-E)UxA/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32WB35C(C-E)UxA.xml
- * CubeMX DB release 6.0.21
+ * CubeMX DB release 6.0.30
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
--- variants/STM32WBxx/WB55C(C-E-G)U/PeripheralPins.c
+++ variants/STM32WBxx/WB55C(C-E-G)U/PeripheralPins.c
@@ -13,7 +13,7 @@
 /*
  * Automatically generated from STM32WB55CCUx.xml, STM32WB55CEUx.xml
  * STM32WB55CGUx.xml
- * CubeMX DB release 6.0.21
+ * CubeMX DB release 6.0.30
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
