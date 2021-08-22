669d7e4a834a3dc748884cacac20398a54e4db70
Paul Melnikov <positron96@gmail.com>
2021-08-22 23:04:57 +0700

    Fix `bitWrite` with parenthesis
    Current version does not wrap arguments in "()", so works incorrectly when used like this:
    ```
    bitWrite(var, bit, cond?1:0);
    ```
    
    
    Same as in here: https://github.com/espressif/arduino-esp32/issues/4466

--- cores/arduino/wiring_constants.h
+++ cores/arduino/wiring_constants.h
@@ -91,7 +91,7 @@ enum BitOrder {
 #define bitRead(value, bit) (((value) >> (bit)) & 0x01)
 #define bitSet(value, bit) ((value) |= (1UL << (bit)))
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#define bitWrite(value, bit, bitvalue) ((bitvalue) ? bitSet((value), (bit)) : bitClear((value), (bit) ))
 
 #define bit(b) (1UL << (b))
 //macro added for compatibility
