10a4ddb48e756a9f80f0b25df599a45bc2c5e836
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2022-01-10 10:05:52 +0100

    core(C0): Enable/disable Backup Domain write protection depending on series
    C0 doesn't have ability to enable/disable Backup Domain write protection
    
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

--- cores/arduino/stm32/backup.h
+++ cores/arduino/stm32/backup.h
@@ -51,10 +51,15 @@ extern "C" {
 #endif /* HID_MAGIC_NUMBER_BKP_VALUE */
 #endif /* BL_HID */
 
+#if defined(HAL_PWR_MODULE_ENABLED) && \
+    (defined(PWR_CR_DBP) || defined(PWR_CR1_DBP) || defined(PWR_DBPR_DBP))
+#define PWR_ENABLE_BACKUP_ACCESS_EXIST
+#endif
+
 /* Exported functions ------------------------------------------------------- */
 static inline void resetBackupDomain(void)
 {
-#ifdef HAL_PWR_MODULE_ENABLED
+#if defined(PWR_ENABLE_BACKUP_ACCESS_EXIST)
   /*  Enable access to the RTC registers */
   HAL_PWR_EnableBkUpAccess();
   /**
@@ -69,7 +74,7 @@ static inline void resetBackupDomain(void)
 
 static inline void enableBackupDomain(void)
 {
-#ifdef HAL_PWR_MODULE_ENABLED
+#if defined(PWR_ENABLE_BACKUP_ACCESS_EXIST)
   /* Allow access to Backup domain */
   HAL_PWR_EnableBkUpAccess();
 #endif
@@ -85,7 +90,7 @@ static inline void enableBackupDomain(void)
 
 static inline void disableBackupDomain(void)
 {
-#ifdef HAL_PWR_MODULE_ENABLED
+#if defined(PWR_ENABLE_BACKUP_ACCESS_EXIST)
   /* Forbid access to Backup domain */
   HAL_PWR_DisableBkUpAccess();
 #endif
