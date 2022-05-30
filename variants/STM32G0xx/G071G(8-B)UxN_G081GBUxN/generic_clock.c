812c4134412bc946a4187937156f272e9648e903
Frederic Pillon <frederic.pillon@st.com>
2022-05-30 17:27:01 +0200

    fix: compilation warnings
    Co-authored-by: Alexandre Bourdiol <50730894+ABOSTM@users.noreply.github.com>

--- variants/STM32G0xx/G071G(8-B)UxN_G081GBUxN/generic_clock.c
+++ variants/STM32G0xx/G071G(8-B)UxN_G081GBUxN/generic_clock.c
@@ -21,8 +21,8 @@
   */
 WEAK void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
 
   /** Configure the main internal regulator output voltage
   */
