b79e0a6e83d3db222383fbbb5a296f313022e168
Alexis Masson <alexis.masson@st.com>
2022-05-30 11:08:50 +0200

    feat: CMake downloads the CMSIS, GCC
    These are mandatory dependencies,
    writing CMake modules to fetch them automatically
    makes for an easier set-up.

---
+++ cmake/ensure_binutils.cmake
@@ -0,0 +1,87 @@
+
+include(FetchContent)
+
+function(ensure_binutils XPACK_PATH AUTODL_XPACK)
+  # not "required" _yet_, if we don't find them we'll try to download them
+  find_program(CMAKE_ASM_COMPILER arm-none-eabi-gcc PATHS ${XPACK_PATH}/bin) # not arm-none-eabi-as, because we want preprocessing
+  find_program(CMAKE_C_COMPILER arm-none-eabi-gcc PATHS ${XPACK_PATH}/bin)
+  find_program(CMAKE_CXX_COMPILER arm-none-eabi-g++ PATHS ${XPACK_PATH}/bin)
+  find_program(CMAKE_AR arm-none-eabi-ar PATHS ${XPACK_PATH}/bin)
+  find_program(CMAKE_LD arm-none-eabi-ld PATHS ${XPACK_PATH}/bin)
+  find_program(CMAKE_OBJCOPY arm-none-eabi-objcopy PATHS ${XPACK_PATH}/bin)
+  find_program(CMAKE_SIZE arm-none-eabi-size PATHS ${XPACK_PATH}/bin)
+
+  if (NOT EXISTS ${CMAKE_ASM_COMPILER} OR NOT EXISTS ${CMAKE_C_COMPILER} OR NOT EXISTS ${CMAKE_CXX_COMPILER} OR NOT EXISTS ${CMAKE_AR} OR NOT EXISTS ${CMAKE_LD} OR NOT EXISTS ${CMAKE_OBJCOPY})
+
+    cmake_host_system_information(
+      RESULT HOSTINFO
+      QUERY OS_NAME OS_PLATFORM
+    )
+    list(GET HOSTINFO 0 HOST_OS)
+    list(GET HOSTINFO 1 HOST_ARCH)
+
+    unset(OSCODE)
+    unset(ARCHIVE_EXT)
+    if (${HOST_OS} STREQUAL "Linux")
+      set(OSCODE "linux")
+      set(ARCHIVE_EXT ".tar.gz")
+    elseif (${HOST_OS} STREQUAL "Windows")
+      set(OSCODE "win32")
+      set(ARCHIVE_EXT ".zip")
+    elseif (${HOST_OS} STREQUAL "Darwin ")
+      set(OSCODE "darwin")
+      set(ARCHIVE_EXT ".tar.gz")
+    endif()
+
+    unset(CPUCODE)
+    string(TOUPPER ${HOST_ARCH} HOST_ARCH)
+    if (${HOST_ARCH} MATCHES "^(AMD64|X86_64|x64)$")
+      set(CPUCODE "x64")
+    elseif (${HOST_ARCH} MATCHES "^(ARM64)$")
+      set(CPUCODE "arm64")
+    elseif (${HOST_ARCH} MATCHES "^(ARM)$")
+      set(CPUCODE "arm")
+    elseif (${HOST_ARCH} MATCHES "^(I386|IA32|x86|i686)$")
+      set(CPUCODE "ia32")
+    endif()
+
+    if (DEFINED OSCODE AND DEFINED CPUCODE AND DEFINED ARCHIVE_EXT)
+      if (${AUTODL_XPACK})
+        FetchContent_Declare(
+          xpack
+          SOURCE_DIR ${XPACK_PATH}
+          URL "https://github.com/xpack-dev-tools/arm-none-eabi-gcc-xpack/releases/download/v10.3.1-2.3/xpack-arm-none-eabi-gcc-10.3.1-2.3-${OSCODE}-${CPUCODE}${ARCHIVE_EXT}"
+        )
+        if (NOT EXISTS ${XPACK_PATH})
+          message(STATUS "Downloading xpack...")
+          FetchContent_MakeAvailable(xpack)
+        endif()
+      else() # supported platform, no autodl
+        message(FATAL_ERROR
+          "
+          Could not find arm-none-eabi-gcc/g++/ar/ld/objcopy. These are required to build for stm32.
+          Please install them and make them available in your $PATH, or let me make my private install using the AUTODL_XPACK option.
+          "
+        )
+      endif()
+    else() # unsupported platform
+      message(FATAL_ERROR
+        "
+        Could not find arm-none-eabi-gcc/g++/ar/ld/objcopy. These are required to build for stm32.
+        Please install them and make them available in your $PATH.
+        "
+      )
+    endif()
+
+
+  endif()
+
+  find_program(CMAKE_ASM_COMPILER arm-none-eabi-gcc PATHS ${XPACK_PATH}/bin REQUIRED)
+  find_program(CMAKE_C_COMPILER arm-none-eabi-gcc PATHS ${XPACK_PATH}/bin REQUIRED)
+  find_program(CMAKE_CXX_COMPILER arm-none-eabi-g++ PATHS ${XPACK_PATH}/bin REQUIRED)
+  find_program(CMAKE_AR arm-none-eabi-ar PATHS ${XPACK_PATH}/bin REQUIRED)
+  find_program(CMAKE_LD arm-none-eabi-ld PATHS ${XPACK_PATH}/bin REQUIRED)
+  find_program(CMAKE_OBJCOPY arm-none-eabi-objcopy PATHS ${XPACK_PATH}/bin REQUIRED)
+  find_program(CMAKE_SIZE arm-none-eabi-size PATHS ${XPACK_PATH}/bin REQUIRED)
+
+endfunction()
--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,15 +1,18 @@
+
+find_program(PYTHON3 "python3")
+find_program(SFDP "sfdp") # graphviz layout engine -- you may change the engine, use dot or whatever
+
+include("${CMAKE_CURRENT_LIST_DIR}/ensure_binutils.cmake")
+
 # Setting Linux is forcing th extension to be .o instead of .obj when building on WIndows.
 # It is important because armlink is failing when files have .obj extensions (error with
 # scatter file section not found)
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
-find_program(CMAKE_ASM_COMPILER arm-none-eabi-gcc REQUIRED) # not arm-none-eabi-as, because we want preprocessing
-find_program(CMAKE_C_COMPILER arm-none-eabi-gcc REQUIRED)
-find_program(CMAKE_CXX_COMPILER arm-none-eabi-g++ REQUIRED)
-find_program(CMAKE_AR arm-none-eabi-ar REQUIRED)
-find_program(CMAKE_LD arm-none-eabi-ld REQUIRED)
-find_program(CMAKE_OBJCOPY arm-none-eabi-objcopy REQUIRED)
+set(XPACK_PATH "${CMAKE_CURRENT_LIST_DIR}/../xpack-arm-none-eabi-gcc")
+option(AUTODL_XPACK OFF)
+ensure_binutils(${XPACK_PATH} ${AUTODL_XPACK})
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) # don't try to link when testing the compiler, it won't work anyway
 set(BUILD_SHARED_LIBS false CACHE STRING "")
