4bc21a5921bf39ec51e4b71397266b7ecd654586
alextrical <35117191+alextrical@users.noreply.github.com>
2022-09-01 11:07:22 +0100

    Update variants/STM32L0xx/L011K(3-4)U_L021K4U/variant_NUCLEO_L011K4.cpp
    Co-authored-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32L0xx/L011K(3-4)U_L021K4U/variant_NUCLEO_L011K4.cpp
+++ variants/STM32L0xx/L011K(3-4)U_L021K4U/variant_NUCLEO_L011K4.cpp
@@ -70,9 +70,9 @@ extern "C" {
   */
 WEAK void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {};
 
   /** Configure the main internal regulator output voltage
   */
