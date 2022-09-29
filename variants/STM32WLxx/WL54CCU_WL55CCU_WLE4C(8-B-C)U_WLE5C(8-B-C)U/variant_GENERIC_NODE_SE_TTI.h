37757faf27be0642d3781b39b051e053e752261e
Frederic Pillon <frederic.pillon@st.com>
2022-09-29 17:20:35 +0200

    variant(WL): update with SUBGHZSPI debug pins
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32WLxx/WL54CCU_WL55CCU_WLE4C(8-B-C)U_WLE5C(8-B-C)U/variant_GENERIC_NODE_SE_TTI.h
+++ variants/STM32WLxx/WL54CCU_WL55CCU_WLE4C(8-B-C)U_WLE5C(8-B-C)U/variant_GENERIC_NODE_SE_TTI.h
@@ -49,6 +49,9 @@
 #define PA1_ALT1                (PA1 | ALT1)
 #define PA2_ALT1                (PA2 | ALT1)
 #define PA3_ALT1                (PA3 | ALT1)
+#define PA4_ALT1                (PA4 | ALT1)
+#define PA5_ALT1                (PA5 | ALT1)
+#define PA6_ALT1                (PA6 | ALT1)
 #define PA7_ALT1                (PA7 | ALT1)
 #define PB8_ALT1                (PB8 | ALT1)
 
@@ -121,6 +124,20 @@
 #endif
 #ifndef PIN_SERIAL_TX
   #define PIN_SERIAL_TX         PA2
+#endif
+
+// Alias
+#ifndef DEBUG_SUBGHZSPI_MOSI
+  #define DEBUG_SUBGHZSPI_MOSI  PA7_ALT1
+#endif
+#ifndef DEBUG_SUBGHZSPI_MISO
+  #define DEBUG_SUBGHZSPI_MISO  PA6_ALT1
+#endif
+#ifndef DEBUG_SUBGHZSPI_SCLK
+  #define DEBUG_SUBGHZSPI_SCLK  PA5_ALT1
+#endif
+#ifndef DEBUG_SUBGHZSPI_SS
+  #define DEBUG_SUBGHZSPI_SS    PA4_ALT1
 #endif
 
 // Extra HAL modules
