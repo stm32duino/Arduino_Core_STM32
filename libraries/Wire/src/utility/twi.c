d6ae8b2d7e4d21f08e893fe3b513f9ff8af11759
Frederic Pillon <frederic.pillon@st.com>
2022-11-05 11:54:50 +0100

    fix(wire): warning about unused parameter
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- libraries/Wire/src/utility/twi.c
+++ libraries/Wire/src/utility/twi.c
@@ -1067,6 +1067,7 @@ void i2c_attachSlaveTxEvent(i2c_t *obj, void (*function)(i2c_t *))
   */
 void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode)
 {
+  UNUSED(AddrMatchCode);
   i2c_t *obj = get_i2c_obj(hi2c);
   if ((obj->slaveMode == SLAVE_MODE_RECEIVE) && (obj->slaveRxNbData != 0)) {
     obj->i2c_onSlaveReceive(obj);
