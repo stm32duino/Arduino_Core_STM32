e5e5c502b79474e12b8c28b975b82f6447ab121c
Frederic Pillon <frederic.pillon@st.com>
2022-12-12 09:46:11 +0100

    fix(ci): wrong BLE readme regex
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- CI/update/stm32cube.py
+++ CI/update/stm32cube.py
@@ -711,7 +711,7 @@ def applyBlePatch():
 def updateBleReadme(filepath, version):
     print(" Updating README.md in ble library")
     for line in fileinput.input(filepath, inplace=True):
-        print(re.sub(r"v\d+.\d+.\d+", version, line), end="")
+        print(re.sub(r"v\d+.\d+.\d+", f"v{version}", line), end="")
 
 
 def updateBleLibrary():
