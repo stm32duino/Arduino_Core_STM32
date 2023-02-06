cfa31c038d2556ebab01bd603f249475c5df58d7
Frederic Pillon <frederic.pillon@st.com>
2023-02-06 17:46:20 +0100

    chore: update wrapped files to ignore register warnings
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/LL/stm32yyxx_ll_comp.h
+++ cores/arduino/stm32/LL/stm32yyxx_ll_comp.h
@@ -4,6 +4,9 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#ifdef __cplusplus
+  #pragma GCC diagnostic ignored "-Wregister"
+#endif
 
 #ifdef STM32F0xx
   #include "stm32f0xx_ll_comp.h"
--- cores/arduino/stm32/LL/stm32yyxx_ll_fsmc.h
+++ cores/arduino/stm32/LL/stm32yyxx_ll_fsmc.h
@@ -4,6 +4,9 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#ifdef __cplusplus
+  #pragma GCC diagnostic ignored "-Wregister"
+#endif
 
 #ifdef STM32F1xx
   #include "stm32f1xx_ll_fsmc.h"
