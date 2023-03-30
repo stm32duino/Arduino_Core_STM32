b74bb7c1e8f233b2c43e816f1c79204bf6ebc264
Brandon Satrom <bsatrom+gh@gmail.com>
2023-03-30 08:42:04 -0500

    name analog pins

--- variants/STM32F4xx/F405RGT_F415RGT/variant_MICROMOD_F405.h
+++ variants/STM32F4xx/F405RGT_F415RGT/variant_MICROMOD_F405.h
@@ -35,67 +35,67 @@
 /*----------------------------------------------------------------------------
  *        Pins
  *----------------------------------------------------------------------------*/
-#define PC0  0  //D0
-#define PC1  1  //D1
+#define PC0  PIN_A1   //D0
+#define PC1  PIN_A12  //D1
 
-#define PC6  2  //PWM0
-#define PC7  3  //PWM1
+#define PC6  2        //PWM0
+#define PC7  3        //PWM1
 
-#define PA2  4  //TX1
-#define PA3  5  //RX1
+#define PA2  PIN_A4   //TX1
+#define PA3  PIN_A5   //RX1
 
-#define PD2  6  //G0
-#define PA8  7  //G1
-#define PA0  8  //G2
-#define PC8  9  //G3
-#define PC9  10  //G4
-#define PC13 11  //G5
-#define PC2  12  //G6
-#define PB13 13  //G10 - HOST_VBUS
-#define PB12 14  //G11 - HOST_ID
+#define PD2  6        //G0
+#define PA8  7        //G1
+#define PA0  PIN_A3   //G2
+#define PC8  9        //G3
+#define PC9  10       //G4
+#define PC13 11       //G5
+#define PC2  PIN_A13  //G6
+#define PB13 13       //G10 - HOST_VBUS
+#define PB12 14       //G11 - HOST_ID
 
-#define PB4  15  //AUD_OUT
-#define PB5  16  //AUD_IN
-#define PA4  17  //AUD_LRCLK
-#define PB3  18  //AUD_BCLK  
+#define PB4  15       //AUD_OUT
+#define PB5  16       //AUD_IN
+#define PA4  PIN_A6   //AUD_LRCLK
+#define PB3  18       //AUD_BCLK
 
-#define PB9  19  //CAN_TX
-#define PB8  20  //CAN_RX
+#define PB9  19       //CAN_TX
+#define PB8  20       //CAN_RX
 
-#define PB10 21  //SCL
-#define PB11 22  //SDA
-#define PB1  23  //INT
+#define PB10 21       //SCL
+#define PB11 22       //SDA
+#define PB1  PIN_A10  //INT
 
-#define PB6  24  //SCL1
-#define PB7  25  //SDA1
+#define PB6  24       //SCL1
+#define PB7  25       //SDA1
 
-#define PA5  26  //SCK
-#define PA7  27  //COPI
-#define PA6  28  //CIPO
-#define PC4  29  //CS
+#define PA5  PIN_A7   //SCK
+#define PA7  PIN_A9   //COPI
+#define PA6  PIN_A8   //CIPO
+#define PC4  PIN_A14  //CS
 
-#define PB14 30  //HOST_D-
-#define PB15 31  //HOST_D+
+#define PB14 30       //HOST_D-
+#define PB15 31       //HOST_D+
 
-#define PA15 32  //STAT LED
+#define PA15 32       //STAT LED
 
 #define PC5  PIN_A0
 #define PB0  PIN_A1
-#define PA1  35  //BATT_VIN/3
+#define PA1  PIN_A2   //BATT_VIN/3
 
-#define PC14 36  //OSC32
-#define PC15 37  //OSC32
+#define PC14 36       //OSC32
+#define PC15 37       //OSC32
 
-#define PA11 38  //D+
-#define PA12 39  //D-
+#define PA11 38       //D+
+#define PA12 39       //D-
 
-#define PA13 40  //SWDIO
-#define PA14 41  //SWCLK
+#define PA13 40       //SWDIO
+#define PA14 41       //SWCLK
 
-#define PC10 42  //FLASH_SCK
-#define PC11 43  //FLASH_CIPO
-#define PC12 44  //FLASH_COPI
-#define PC3  45  //FLASH_CS
+#define PC10 42       //FLASH_SCK
+#define PC11 43       //FLASH_CIPO
+#define PC12 44       //FLASH_COPI
+#define PC3  45       //FLASH_CS
 
 // This must be a literal
 #define NUM_DIGITAL_PINS        46
