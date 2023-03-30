0a97cabe5622e9caa5931b2b2f3c35a47a7db5be
Brandon Satrom <bsatrom+gh@gmail.com>
2023-03-30 14:45:28 -0500

    fix inadvertant ! in #if on PeripheralPins

--- variants/STM32F4xx/F405RGT_F415RGT/PeripheralPins_MICROMOD_F405.c
+++ variants/STM32F4xx/F405RGT_F415RGT/PeripheralPins_MICROMOD_F405.c
@@ -14,7 +14,7 @@
  * Automatically generated from STM32F405RGTx.xml, STM32F415RGTx.xml
  * CubeMX DB release 6.0.80
  */
-#if !defined(ARDUINO_MICROMOD_F405)
+#if defined(ARDUINO_MICROMOD_F405)
 #include "Arduino.h"
 #include "PeripheralPins.h"
 
