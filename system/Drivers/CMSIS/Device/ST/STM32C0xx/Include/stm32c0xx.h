4d6f76db19b7f9537f5fe950538233b5ccdc3234
Frederic Pillon <frederic.pillon@st.com>
2023-07-13 09:17:03 +0200

    system(C0): update STM32C0xx CMSIS Drivers to v1.1.0
    Included in STM32CubeC0 FW v1.1.0
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- system/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h
+++ system/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h
@@ -77,7 +77,7 @@
   * @brief CMSIS Device version number V1.0.0
   */
 #define __STM32C0_CMSIS_VERSION_MAIN   (0x01U) /*!< [31:24] main version */
-#define __STM32C0_CMSIS_VERSION_SUB1   (0x00U) /*!< [23:16] sub1 version */
+#define __STM32C0_CMSIS_VERSION_SUB1   (0x01U) /*!< [23:16] sub1 version */
 #define __STM32C0_CMSIS_VERSION_SUB2   (0x00U) /*!< [15:8]  sub2 version */
 #define __STM32C0_CMSIS_VERSION_RC     (0x00U) /*!< [7:0]  release candidate */
 #define __STM32C0_CMSIS_VERSION        ((__STM32C0_CMSIS_VERSION_MAIN << 24)\
