f2f54edca2fdd5681d8db3f0b9e747e299a4eb8a
Frederic Pillon <frederic.pillon@st.com>
2023-07-13 15:12:09 +0200

    core(U5): update wrapped files
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- libraries/SrcWrapper/src/HAL/stm32yyxx_hal_gfxtim.c
+++ libraries/SrcWrapper/src/HAL/stm32yyxx_hal_gfxtim.c
@@ -2,11 +2,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-#ifdef STM32F7xx
-  #include "stm32f7xx_hal_jpeg.c"
-#elif STM32H7xx
-  #include "stm32h7xx_hal_jpeg.c"
-#elif STM32U5xx
-  #include "stm32u5xx_hal_jpeg.c"
+#ifdef STM32U5xx
+  #include "stm32u5xx_hal_gfxtim.c"
 #endif
 #pragma GCC diagnostic pop
