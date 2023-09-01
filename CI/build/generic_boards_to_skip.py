abc3e61ef69678bb0c771e348c4aa1ab083f0b8b
Frederic Pillon <frederic.pillon@st.com>
2023-09-01 10:26:20 +0200

    ci(generic_board_to_skip): update dynamically config files
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- CI/build/generic_boards_to_skip.py
+++ CI/build/generic_boards_to_skip.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pathlib import Path
 
 
@@ -7,12 +8,47 @@ script_path = Path(__file__).parent.resolve()
 core_path = script_path.parent.parent
 variant_path = core_path / "variants"
 boards_entry_filename = "boards_entry.txt"
-output_filemane = script_path / "generic_boards_to_skip.json"
+# Config files
+conf_path = script_path / "conf"
+cores_file = conf_path / "cores_config.json"
+ci_file = conf_path / "cores_config_ci.json"
 
 # Parser
 parser = argparse.ArgumentParser(
     description="Generate list of generic boards to skip for ci core config"
 )
+
+
+def update_config(config_file, boards_list):
+    if not config_file.is_file():
+        print(f"{config_file} not found!")
+        exit(1)
+    try:
+        config = open(config_file, "r")
+        json_data = json.load(config)
+        config.close()
+        # Update the values for generic boards
+        # First search correct value
+        core_index = -1
+        sketch_index = -1
+        for index, core in enumerate(json_data["cores"]):
+            if core["architecture"] == "stm32":
+                core_index = index
+                for index2, sketch in enumerate(core["sketches"]):
+                    if sketch["pattern"] == "^.*$":
+                        sketch_index = index2
+        old_boards_list = json_data["cores"][core_index]["sketches"][sketch_index][
+            "boards"
+        ]
+        old_boards_list = [x for x in old_boards_list if not x.startswith("GENERIC_")]
+        new_boards_list = old_boards_list + boards_list
+        new_boards_list.sort()
+        json_data["cores"][core_index]["sketches"][index]["boards"] = new_boards_list
+        config = open(config_file, "w")
+        config.write(json.dumps(json_data, indent=2))
+        config.close()
+    except IOError:
+        print(f"Failed to open {config_file}!")
 
 
 def main():
@@ -31,18 +67,12 @@ def main():
                         boards_list.append(line.partition("=")[-1].strip())
             # Remove last board of the boards_entry to not skip it
             boards_list.pop()
-    # Create file
-    try:
-        output_file = open(output_filemane, "w", newline="\n")
-        for count, board in enumerate(sorted(boards_list), start=1):
-            if count % 4 == 0:
-                output_file.write(f'"{board}",\n')
-            else:
-                output_file.write(f'"{board}", ')
-        output_file.close()
-    except IOError:
-        print(f"Failed to open {output_filemane}")
-        exit(1)
+    boards_list.sort()
+
+    # Update each config files
+    update_config(ci_file, boards_list)
+    update_config(cores_file, boards_list)
+
     exit(0)
 
 
