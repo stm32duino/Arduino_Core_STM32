8d0744678979688cb763fe3c063dc4015db659df
Alexandr Zarubkin <me21@yandex.ru>
2023-11-02 10:50:26 +0300

    Update EEPROM.h
    Disable auto flush temporarily when writing large objects to improve performance and reduce flash wear.
    
    Signed-off-by: Alexandr Zarubkin <me21@yandex.ru>

--- libraries/EEPROM/src/EEPROM.h
+++ libraries/EEPROM/src/EEPROM.h
@@ -240,9 +240,16 @@ struct EEPROMClass {
   {
     EEPtr e = idx;
     const uint8_t *ptr = (const uint8_t *) &t;
+#if !defined(DATA_EEPROM_BASE)
+    eeprom_disable_auto_flush();
+#endif
     for (int count = sizeof(T) ; count ; --count, ++e) {
       (*e).update(*ptr++);
     }
+#if !defined(DATA_EEPROM_BASE)
+    eeprom_buffer_flush();
+    eeprom_enable_auto_flush();
+#endif
     return t;
   }
 };
