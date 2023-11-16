64bea5ce8e8264a1af66ffa1bbe78443f08389a6
Frederic Pillon <frederic.pillon@st.com>
2023-11-16 16:15:56 +0100

    chore: update core version to 2.8.0-dev (0x020800F0)
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/stm32_def.h
+++ cores/arduino/stm32/stm32_def.h
@@ -6,15 +6,15 @@
  * @brief STM32 core version number
  */
 #define STM32_CORE_VERSION_MAJOR    (0x02U) /*!< [31:24] major version */
-#define STM32_CORE_VERSION_MINOR    (0x07U) /*!< [23:16] minor version */
-#define STM32_CORE_VERSION_PATCH    (0x01U) /*!< [15:8]  patch version */
+#define STM32_CORE_VERSION_MINOR    (0x08U) /*!< [23:16] minor version */
+#define STM32_CORE_VERSION_PATCH    (0x00U) /*!< [15:8]  patch version */
 /*
  * Extra label for development:
  * 0: official release
  * [1-9]: release candidate
  * F[0-9]: development
  */
-#define STM32_CORE_VERSION_EXTRA    (0x00U) /*!< [7:0]  extra version */
+#define STM32_CORE_VERSION_EXTRA    (0xF0U) /*!< [7:0]  extra version */
 #define STM32_CORE_VERSION          ((STM32_CORE_VERSION_MAJOR << 24U)\
                                         |(STM32_CORE_VERSION_MINOR << 16U)\
                                         |(STM32_CORE_VERSION_PATCH << 8U )\
