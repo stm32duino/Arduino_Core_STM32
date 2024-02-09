d00afd93fa735004ebe5c69f12203f87b706bcc8
Frederic Pillon <frederic.pillon@st.com>
2024-02-09 17:16:26 +0100

    core(WBA): add top HAL include
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/stm32_def.h
+++ cores/arduino/stm32/stm32_def.h
@@ -58,6 +58,8 @@
   #include "stm32u5xx.h"
 #elif defined(STM32WBxx)
   #include "stm32wbxx.h"
+#elif defined(STM32WBAxx)
+  #include "stm32wbaxx.h"
 #elif defined(STM32WLxx)
   #include "stm32wlxx.h"
 #else
