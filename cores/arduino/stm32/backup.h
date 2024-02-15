c645cc5a2faeaa4b7a6a1211d171f9e8c3f0f6d9
Frederic Pillon <frederic.pillon@st.com>
2024-02-15 16:17:26 +0100

    core(WBA): Backup register API update
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cores/arduino/stm32/backup.h
+++ cores/arduino/stm32/backup.h
@@ -120,7 +120,8 @@ static inline void setBackupRegister(uint32_t index, uint32_t value)
   LL_RTC_BAK_SetRegister(RTC, index, value);
 #elif defined(TAMP_BKP0R)
 #if defined(STM32G4xx) || defined(STM32H5xx) || defined(STM32L5xx) ||\
-    defined(STM32U5xx) || defined(STM32MP1xx) || defined(STM32WLxx)
+    defined(STM32U5xx) || defined(STM32MP1xx) || defined(STM32WBAxx) ||\
+    defined(STM32WLxx)
   /* For those series this API requires RTC even if it is not used
      and TAMP is used instead */
   LL_RTC_BKP_SetRegister(RTC, index, value);
@@ -143,7 +144,8 @@ static inline uint32_t getBackupRegister(uint32_t index)
   return LL_RTC_BAK_GetRegister(RTC, index);
 #elif defined(TAMP_BKP0R)
 #if defined(STM32G4xx) || defined(STM32H5xx) || defined(STM32L5xx) ||\
-    defined(STM32U5xx) || defined(STM32MP1xx) || defined(STM32WLxx)
+    defined(STM32U5xx) || defined(STM32MP1xx) || defined(STM32WBAxx) ||\
+    defined(STM32WLxx)
   /* For those series this API requires RTC even if it is not used
      and TAMP is used instead */
   return LL_RTC_BKP_GetRegister(RTC, index);
