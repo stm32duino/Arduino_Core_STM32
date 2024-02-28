b4dd5f9c13c65a1d35a742f87181c294f75d1f58
Frederic Pillon <frederic.pillon@st.com>
2024-02-28 14:35:14 +0100

    refactor(i2c): simplify constructor
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- libraries/Wire/src/Wire.cpp
+++ libraries/Wire/src/Wire.cpp
@@ -33,18 +33,6 @@ static const uint8_t MASTER_ADDRESS = 0x01;
 
 // Constructors ////////////////////////////////////////////////////////////////
 
-TwoWire::TwoWire()
-{
-  memset((void *)&_i2c, 0, sizeof(_i2c));
-  _i2c.sda = digitalPinToPinName(SDA);
-  _i2c.scl = digitalPinToPinName(SCL);
-
-  txBuffer = nullptr;
-  txBufferAllocated = 0;
-  rxBuffer = nullptr;
-  rxBufferAllocated = 0;
-}
-
 TwoWire::TwoWire(uint32_t sda, uint32_t scl)
 {
   memset((void *)&_i2c, 0, sizeof(_i2c));
