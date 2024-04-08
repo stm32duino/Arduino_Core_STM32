b839de3b00d38fb5ac401f5d698a3a5a99517b04
Frederic Pillon <frederic.pillon@st.com>
2024-04-08 09:28:36 +0200

    chore(h5): update hal conf default
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- system/STM32H5xx/stm32h5xx_hal_conf_default.h
+++ system/STM32H5xx/stm32h5xx_hal_conf_default.h
@@ -148,7 +148,7 @@ The real value may vary depending on the variations
 in voltage and temperature.*/
 
 #if !defined  (LSI_STARTUP_TIME)
-#define LSI_STARTUP_TIME          130UL      /*!< Time out for LSI start up, in ms */
+#define LSI_STARTUP_TIME          130UL      /*!< Time out for LSI start up, in us */
 #endif /* LSI_STARTUP_TIME */
 
 /**
