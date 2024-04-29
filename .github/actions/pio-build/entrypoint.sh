4782f07d49daaa13d541087116689c66fce3df91
Frederic Pillon <frederic.pillon@st.com>
2024-04-29 14:34:33 +0200

    fix(pio-build): use python venv
    See https://peps.python.org/pep-0668/
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- .github/actions/pio-build/entrypoint.sh
+++ .github/actions/pio-build/entrypoint.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 readonly CMSIS_VERSION="$1"
 readonly CMSIS_ARCHIVE="CMSIS-${CMSIS_VERSION}.tar.bz2"
+
+# Use python venv
+python3 -m venv "$HOME/venv"
+. "$HOME/venv/bin/activate"
+# Install PlatformIO
+python3 -m pip install --quiet --upgrade platformio
 
 # Install the development version of ststm32 platform
 platformio platform install "https://github.com/platformio/platform-ststm32.git" || {
