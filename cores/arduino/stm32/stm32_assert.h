000dad7f67303b3be90f59b8c173c4de20b0bcfd
Frederic Pillon <frederic.pillon@st.com>
2024-05-23 10:46:56 +0200

    feat: support stm32 assert
    Fixes #2375.
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

---
+++ cores/arduino/stm32/stm32_assert.h
@@ -0,0 +1,10 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef STM32_ASSERT_H
+#define STM32_ASSERT_H
+
+/*
+ * Header file required by LL layer but as USE_HAL_DRIVER is defined
+ * assert definitions are provided by stm32yyxx_hal_conf_default
+ */
+
+#endif /* STM32_ASSERT_H */
