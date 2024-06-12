eee321f131aebfcd96be4029575bb5cb5448aa61
Frederic Pillon <frederic.pillon@st.com>
2024-06-12 18:21:56 +0200

    fix(cmake): support extern "C" when generate header
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cmake/scripts/generate_header.py
+++ cmake/scripts/generate_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
@@ -8,8 +9,7 @@ parser.add_argument("--source", "-i", type=Path, required=True, help="ctags's ou
 parser.add_argument("--out", "-o", type=Path, required=True, help="header to generate")
 
 shargs = parser.parse_args()
-
-
+externC_regex = re.compile(r'extern\s+"C"')
 with open(shargs.source, "r") as infile:
     with open(shargs.out, "w") as outfile:
         for line in infile:
@@ -20,7 +20,11 @@ with open(shargs.source, "r") as infile:
             fields = line.split("\t")
             kind = fields[3].split(":", 1)[1]
             if kind == "function":
+                decl = ""
+                externC = externC_regex.search(fields[2])
+                if externC:
+                    decl = 'extern "C" '
                 symname = fields[0]
                 signature = fields[5].split(":", 1)[1]
                 rtype = fields[6].split(":", 1)[1]
-                print(f"{rtype} {symname}{signature};", file=outfile)
+                print(f"{decl}{rtype} {symname}{signature};", file=outfile)
