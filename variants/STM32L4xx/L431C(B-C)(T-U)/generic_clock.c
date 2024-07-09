18df7f38da6d1052440a3e545b38466c8d1fba3e
Frederic Pillon <frederic.pillon@st.com>
2024-07-09 15:09:04 +0200

    Update variants/STM32L4xx/L431C(B-C)(T-U)/generic_clock.c
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32L4xx/L431C(B-C)(T-U)/generic_clock.c
+++ variants/STM32L4xx/L431C(B-C)(T-U)/generic_clock.c
@@ -50,7 +50,7 @@ WEAK void SystemClock_Config(void)
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                              | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+                                | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
