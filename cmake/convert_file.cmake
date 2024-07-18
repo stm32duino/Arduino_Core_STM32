8554e3e08a349807ccd1c90241fea61a8e35317c
Frederic Pillon <frederic.pillon@st.com>
2024-07-18 10:56:19 +0200

    fix(cmake): remove .elf extension from generated .bin and .hex
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- cmake/convert_file.cmake
+++ cmake/convert_file.cmake
@@ -1,16 +1,18 @@
 cmake_minimum_required(VERSION 3.21)
 function(elf2bin ELFTGT)
+  get_filename_component(ELFTGT_WE ${ELFTGT} NAME_WE)
   add_custom_command(TARGET ${ELFTGT} POST_BUILD
-    COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${ELFTGT}> $<TARGET_FILE:${ELFTGT}>.bin
+    COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${ELFTGT}> $<TARGET_FILE_DIR:${ELFTGT}>/${ELFTGT_WE}.bin
   )
-  set_property(TARGET ${ELFTGT} APPEND PROPERTY ADDITIONAL_CLEAN_FILES "$<TARGET_FILE:${ELFTGT}>.bin")
+  set_property(TARGET ${ELFTGT} APPEND PROPERTY ADDITIONAL_CLEAN_FILES "$<TARGET_FILE_DIR:${ELFTGT}>/${ELFTGT_WE}.bin")
 endfunction()
 
 function(elf2hex ELFTGT)
+  get_filename_component(ELFTGT_WE ${ELFTGT} NAME_WE)
   add_custom_command(TARGET ${ELFTGT} POST_BUILD
-    COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${ELFTGT}> $<TARGET_FILE:${ELFTGT}>.hex
+    COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${ELFTGT}> $<TARGET_FILE_DIR:${ELFTGT}>/${ELFTGT_WE}.hex
   )
-  set_property(TARGET ${ELFTGT} APPEND PROPERTY ADDITIONAL_CLEAN_FILES "$<TARGET_FILE:${ELFTGT}>.hex")
+  set_property(TARGET ${ELFTGT} APPEND PROPERTY ADDITIONAL_CLEAN_FILES "$<TARGET_FILE_DIR:${ELFTGT}>/${ELFTGT_WE}.hex")
 endfunction()
 
 function(gv2svg GVFILE ENGINE)
