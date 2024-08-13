4ee467b7d118145db0ec3a797fffd58c693c497b
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2024-08-13 12:37:39 +0200

    Update: IWDG_Button now use Serial (USB) to print information and interrupt for button

--- libraries/Other_Examples/examples/IWatchdog/examples/IWDG_Button/IWDG_Button.ino
+++ libraries/Other_Examples/examples/IWatchdog/examples/IWDG_Button/IWDG_Button.ino
@@ -1,46 +1,48 @@
 /*
-  IWatchdog + Button
-
-  This example code is in the public domain.
 
   The code demonstrates the use of a independent watchdog timer.
   The watchdog timer is initialized with timeout of 10 seconds.
-  Every time the button is pressed, the watchdog timer is reset.
+  Every time the button is pressed, the watchdog timer is reset on button release.
   If left unattended the system would reset itself about every 10 seconds.
 
   You would have to keep pressing the button frequently (< 10 seconds apart)
   to prevent the system from resetting itself.
 
   You would recognize the reset condition when the LED blinks few times quickly.
-
-  This is not a practical example, in real code you would reset the watchdog
-  timer in the main loop without requiring user input.
-
-  The code is modified version of the code from:
-  http://www.arduino.cc/en/Tutorial/Button
 */
 
 #include <IWatchdog.h>
 
 #ifdef USER_BTN
 const int buttonPin = USER_BTN;
-#else
-const int buttonPin = 2;
 #endif
 
 #ifdef LED_BUILTIN
 const int ledPin = LED_BUILTIN;
-#else
-const int ledPin = 13;
 #endif
 
-static int default_buttonState = LOW;
+#define BUTTON_DEBOUNCE_TIME_MS 50 // 50 milliseconds
+volatile uint32_t button_release_last_evt_timestamp;
+volatile bool button_release_flag;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  Serial.begin(115200);
+  while (!Serial) {}
 
+  Serial.println("IWDG_Button demo");
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), button_release_isr, RISING);
+
+  // Check if the current reset cause by IWatchdog
   if (IWatchdog.isReset(true)) {
+    Serial.println("Reset by IWatchdog detected");
+    
     // LED blinks to indicate reset
     for (uint8_t idx = 0; idx < 5; idx++) {
       digitalWrite(ledPin, HIGH);
@@ -50,36 +52,47 @@ void setup() {
     }
   }
 
-  // Read default state of the pushbutton
-  default_buttonState = digitalRead(buttonPin);
-
   // Init the watchdog timer with 10 seconds timeout
   IWatchdog.begin(10000000);
-  // or with a 2 seconds window
-  // IWatchdog.begin(10000000, 2000000);
 
+  // Check IWDG init status
   if (!IWatchdog.isEnabled()) {
     // LED blinks indefinitely
     while (1) {
+      Serial.println("Init FAIL");
+
       digitalWrite(ledPin, HIGH);
       delay(500);
       digitalWrite(ledPin, LOW);
       delay(500);
     }
   }
+
+  Serial.println("Init DONE");
 }
 
 void loop() {
-  // Compare current button state of the pushbutton value:
-  if (digitalRead(buttonPin) == default_buttonState) {
-    digitalWrite(ledPin, LOW);
-  } else {
+  if (button_release_flag) {
+    Serial.println("Button press detected. Reload IWatchdog.");
+
+    // Blink LED to indicate IWDG reloading
     digitalWrite(ledPin, HIGH);
+    delay(150);
+    digitalWrite(ledPin, LOW);
 
-    // Uncomment to change timeout value to 6 seconds
-    //IWatchdog.set(6000000);
-
-	// Reload the watchdog only when the button is pressed
+    // Reload the IWDG
     IWatchdog.reload();
+
+    // Clear button flag
+    button_release_flag = false;
+  }
+}
+
+void button_release_isr(void)
+{
+  if (millis() - button_release_last_evt_timestamp > BUTTON_DEBOUNCE_TIME_MS)
+  {
+    button_release_last_evt_timestamp = millis();
+    button_release_flag = true;
   }
 }
