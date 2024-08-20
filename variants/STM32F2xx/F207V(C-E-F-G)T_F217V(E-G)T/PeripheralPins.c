250d2393bbc00051dbcc99a6d99116103458779b
Frederic Pillon <frederic.pillon@st.com>
2024-08-20 09:27:20 +0200

    chore(f2): update variants against CubeMX DB release 6.0.120
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32F2xx/F207V(C-E-F-G)T_F217V(E-G)T/PeripheralPins.c
+++ variants/STM32F2xx/F207V(C-E-F-G)T_F217V(E-G)T/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32F207V(C-E-F-G)Tx.xml, STM32F217V(E-G)Tx.xml
- * CubeMX DB release 6.0.110
+ * CubeMX DB release 6.0.120
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
