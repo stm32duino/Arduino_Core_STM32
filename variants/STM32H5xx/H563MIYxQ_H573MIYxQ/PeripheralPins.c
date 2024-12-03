62168c06fc0d7c85384193a13556f768e92130fc
Frederic Pillon <frederic.pillon@st.com>
2024-12-03 14:58:15 +0100

    chore(h5): update variants against CubeMX DB release 6.0.130
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32H5xx/H563MIYxQ_H573MIYxQ/PeripheralPins.c
+++ variants/STM32H5xx/H563MIYxQ_H573MIYxQ/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32H563MIYxQ.xml, STM32H573MIYxQ.xml
- * CubeMX DB release 6.0.120
+ * CubeMX DB release 6.0.130
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
