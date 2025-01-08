fe7fb43e77ffd23420027e0f7893f4f88f7691a3
Frederic Pillon <frederic.pillon@st.com>
2025-01-08 14:42:46 +0100

    chore(wl): update variant after script update
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32WLxx/WL54CCU_WL55CCU_WLE4C(8-B-C)U_WLE5C(8-B-C)U/PinNamesVar.h
+++ variants/STM32WLxx/WL54CCU_WL55CCU_WLE4C(8-B-C)U_WLE5C(8-B-C)U/PinNamesVar.h
@@ -18,20 +18,5 @@ PB_8_ALT1 = PB_8 | ALT1,
 #ifdef PWR_WAKEUP_PIN3
   SYS_WKUP3 = PB_3,
 #endif
-#ifdef PWR_WAKEUP_PIN4
-  SYS_WKUP4 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN5
-  SYS_WKUP5 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN6
-  SYS_WKUP6 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN7
-  SYS_WKUP7 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN8
-  SYS_WKUP8 = NC,
-#endif
 
 /* No USB */
