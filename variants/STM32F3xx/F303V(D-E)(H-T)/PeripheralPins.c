1aaf0d06e39101c9b8d8b6fdf63f785af517aa13
Frederic Pillon <frederic.pillon@st.com>
2025-01-08 14:27:05 +0100

    chore(f3): update variant after script update
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- variants/STM32F3xx/F303V(D-E)(H-T)/PeripheralPins.c
+++ variants/STM32F3xx/F303V(D-E)(H-T)/PeripheralPins.c
@@ -12,7 +12,7 @@
  */
 /*
  * Automatically generated from STM32F303V(D-E)Hx.xml, STM32F303V(D-E)Tx.xml
- * CubeMX DB release 6.0.120
+ * CubeMX DB release 6.0.130
  */
 #if !defined(CUSTOM_PERIPHERAL_PINS)
 #include "Arduino.h"
--- variants/STM32F3xx/F303V(D-E)(H-T)/PinNamesVar.h
+++ variants/STM32F3xx/F303V(D-E)(H-T)/PinNamesVar.h
@@ -69,21 +69,6 @@ PF_2_ALT1  = PF_2  | ALT1,
 #ifdef PWR_WAKEUP_PIN3
   SYS_WKUP3 = PE_6,
 #endif
-#ifdef PWR_WAKEUP_PIN4
-  SYS_WKUP4 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN5
-  SYS_WKUP5 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN6
-  SYS_WKUP6 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN7
-  SYS_WKUP7 = NC,
-#endif
-#ifdef PWR_WAKEUP_PIN8
-  SYS_WKUP8 = NC,
-#endif
 
 /* USB */
 #ifdef USBCON
