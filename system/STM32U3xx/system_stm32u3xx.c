561729c3405d980fea3dfc0dce58606fd98559bf
Frederic Pillon <frederic.pillon@st.com>
2025-02-06 10:29:50 +0100

    system(u3): update STM32U3xx system
    Remove duplicate clock definition.
    Allow VECT_TAB_OFFSET redefinition.
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- system/STM32U3xx/system_stm32u3xx.c
+++ system/STM32U3xx/system_stm32u3xx.c
@@ -82,45 +82,28 @@
 /** @addtogroup STM32U3xx_System_Private_Defines
   * @{
   */
-#if !defined  (HSE_VALUE)
-  #define HSE_VALUE             32000000U /*!< Value of the External oscillator in Hz */
-#endif /* HSE_VALUE */
-
-#if !defined  (HSI_VALUE)
-  #define HSI_VALUE             16000000U /*!< Value of the Internal oscillator in Hz*/
-#endif /* HSI_VALUE */
-
-#if !defined  (MSIRC0_VALUE)
-  #define MSIRC0_VALUE          96000000U /*!< Value of the Internal MSI RC0 oscillator in Hz*/
-#endif /* MSI_VALUE */
-
-#if !defined  (MSIRC1_VALUE)
-  #define MSIRC1_VALUE          24000000U /*!< Value of the Internal MSI RC1 oscillator in Hz*/
-#endif /* MSI_VALUE */
 
 /* Note: Following vector table addresses must be defined in line with linker
          configuration. */
-/*!< Uncomment the following line if you need to relocate the vector table
-     anywhere in Flash or Sram, else the vector table is kept at the automatic
-     remap of boot address selected */
-/*#define USER_VECT_TAB_ADDRESS */
-
-#if defined(USER_VECT_TAB_ADDRESS)
 /*!< Uncomment the following line if you need to relocate your vector Table
      in Sram else user remap will be done in Flash. */
 /*#define VECT_TAB_SRAM */
+
+#ifndef VECT_TAB_OFFSET
+#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table base offset field.
+                                                     This value must be a multiple of 0x200. */
+#endif /* VECT_TAB_OFFSET */
+
+#ifndef VECT_TAB_BASE_ADDRESS
 #if defined(VECT_TAB_SRAM)
 #define VECT_TAB_BASE_ADDRESS   SRAM1_BASE      /*!< Vector Table base address field.
-                                                     This value must be a multiple of 0x200. */
-#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table base offset field.
                                                      This value must be a multiple of 0x200. */
 #else
 #define VECT_TAB_BASE_ADDRESS   FLASH_BASE      /*!< Vector Table base address field.
                                                      This value must be a multiple of 0x200. */
-#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table base offset field.
-                                                     This value must be a multiple of 0x200. */
+
 #endif /* VECT_TAB_SRAM */
-#endif /* USER_VECT_TAB_ADDRESS */
+#endif /* VECT_TAB_BASE_ADDRESS */
 
 /******************************************************************************/
 
@@ -179,9 +162,7 @@ void SystemInit(void)
 #endif
 
   /* Configure the Vector Table location -------------------------------------*/
-#if defined(USER_VECT_TAB_ADDRESS)
   SCB->VTOR = VECT_TAB_BASE_ADDRESS | VECT_TAB_OFFSET; /* Vector Table Relocation */
-#endif /* USER_VECT_TAB_ADDRESS */
 }
 
 /**
