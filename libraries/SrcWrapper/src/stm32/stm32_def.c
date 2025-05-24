8178fad4b4f65ecf36b2ffdefc4f87d8f121f806
Avamander <avamander@gmail.com>
2025-05-25 02:25:29 +0300

    Annotate Error_Handler with noreturn to help analysis
    Signed-off-by: Avamander <avamander@gmail.com>

--- libraries/SrcWrapper/src/stm32/stm32_def.c
+++ libraries/SrcWrapper/src/stm32/stm32_def.c
@@ -11,7 +11,7 @@ extern "C" {
   * @retval None
   */
 #if !defined(NDEBUG)
-WEAK void _Error_Handler(const char *msg, int val)
+__attribute__((noreturn)) WEAK void _Error_Handler(const char *msg, int val)
 {
   /* User can add his own implementation to report the HAL error return state */
   core_debug("Error: %s (%i)\n", msg, val);
