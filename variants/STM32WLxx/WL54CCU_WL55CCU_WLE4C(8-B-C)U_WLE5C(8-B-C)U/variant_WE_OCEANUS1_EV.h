6508cb3179ebfba2f85ee0979c7da30595e60e60
wm-eisos <wajdi.elmuhtadi@we-online.com>
2025-06-17 10:01:03 +0200

    variant(WL): add Oceanus-I module and EV
    Signed-off-by: wm-eisos <wajdi.elmuhtadi@we-online.com>

---
+++ variants/STM32WLxx/WL54CCU_WL55CCU_WLE4C(8-B-C)U_WLE5C(8-B-C)U/variant_WE_OCEANUS1_EV.h
@@ -0,0 +1,22 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2020, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+#pragma once
+
+#include "variant_WE_OCEANUS1.h"
+
+// On-board LED pin number
+#define LED_BUILTIN           PA7
+
+// On-board user button
+#define USER_BTN              PA0
+
