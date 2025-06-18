febe8c06563bffc24e34c42e118c966e6b8aa5be
Frederic Pillon <frederic.pillon@st.com>
2025-06-18 14:40:29 +0200

    variants(wb0): add all generated STM32WB0x generic variant files
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

---
+++ variants/STM32WB0x/WB06KCV_WB07KCV/PinNamesVar.h
@@ -0,0 +1,53 @@
+/* No alternate */
+
+/* SYS_WKUP */
+#ifdef PWR_WAKEUP_PB0
+  SYS_WKUP1 = PB_0, /* SYS_WKUP0 */
+#endif
+#ifdef PWR_WAKEUP_PB1
+  SYS_WKUP2 = PB_1, /* SYS_WKUP1 */
+#endif
+#ifdef PWR_WAKEUP_PB2
+  SYS_WKUP3 = PB_2, /* SYS_WKUP2 */
+#endif
+#ifdef PWR_WAKEUP_PB3
+  SYS_WKUP4 = PB_3, /* SYS_WKUP3 */
+#endif
+#ifdef PWR_WAKEUP_PB4
+  SYS_WKUP5 = PB_4, /* SYS_WKUP4 */
+#endif
+#ifdef PWR_WAKEUP_PB5
+  SYS_WKUP6 = PB_5, /* SYS_WKUP5 */
+#endif
+#ifdef PWR_WAKEUP_PB6
+  SYS_WKUP7 = PB_6, /* SYS_WKUP6 */
+#endif
+#ifdef PWR_WAKEUP_PB7
+  SYS_WKUP8 = PB_7, /* SYS_WKUP7 */
+#endif
+#ifdef PWR_WAKEUP_PA8
+  SYS_WKUP9 = PA_8, /* SYS_WKUP8 */
+#endif
+#ifdef PWR_WAKEUP_PA9
+  SYS_WKUP10 = PA_9, /* SYS_WKUP9 */
+#endif
+#ifdef PWR_WAKEUP_PA10
+  SYS_WKUP11 = PA_10, /* SYS_WKUP10 */
+#endif
+#ifdef PWR_WAKEUP_PA11
+  SYS_WKUP12 = PA_11, /* SYS_WKUP11 */
+#endif
+#ifdef PWR_WAKEUP_PA0
+  SYS_WKUP13 = PA_0, /* SYS_WKUP12 */
+#endif
+#ifdef PWR_WAKEUP_PA1
+  SYS_WKUP14 = PA_1, /* SYS_WKUP13 */
+#endif
+#ifdef PWR_WAKEUP_PA2
+  SYS_WKUP15 = PA_2, /* SYS_WKUP14 */
+#endif
+#ifdef PWR_WAKEUP_PA3
+  SYS_WKUP16 = PA_3, /* SYS_WKUP15 */
+#endif
+
+/* No USB */
