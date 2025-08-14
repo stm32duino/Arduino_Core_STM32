2bbc2d8c11c21556e5cf8d980e2234c84e4589a8
Nessotrin <nessotrin@hotmail.fr>
2025-08-14 05:48:13 +0200

    fix(c0): missing GPIO_AF0_USART2 for STM32C051xx
    See https://github.com/STMicroelectronics/stm32c0xx-hal-driver/issues/4
    
    Signed-off-by: Nessotrin <nessotrin@hotmail.fr>
    Co-authored-by: Frederic Pillon <frederic.pillon@st.com>

--- libraries/SrcWrapper/inc/stm32_def.h
+++ libraries/SrcWrapper/inc/stm32_def.h
@@ -222,9 +222,14 @@ __STATIC_INLINE void LL_RTC_SetBinMixBCDU(RTC_TypeDef *RTCx, uint32_t BinMixBcdU
   #define GPIO_AF1_SPI1 STM_PIN_AFNUM_MASK
 #endif
 
-#if defined(STM32C0xx) && defined(USART3) && !defined(GPIO_AF7_USART3)
-  #define GPIO_AF7_USART3 ((uint8_t)0x07)
-#endif // STM32C0xx && !defined(USART3)
+#if defined(STM32C0xx)
+  #if defined(USART3) && !defined(GPIO_AF7_USART3)
+    #define GPIO_AF7_USART3 ((uint8_t)0x07)
+  #endif /* USART3 & !GPIO_AF7_USART3*/
+  #if defined(STM32C051xx) && !defined(GPIO_AF0_USART2)
+    #define GPIO_AF0_USART2 ((uint8_t)0x00)
+  #endif
+#endif // STM32C0xx
 
 #if defined(STM32WBAxx) && defined(USB_OTG_HS) && !defined(GPIO_AF4_USB_OTG_HS)
   #define GPIO_AF4_USB_OTG_HS GPIO_AF4_OTG_HS
