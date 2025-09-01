e96c3a11fb51b1d1e32120555b8863d9febb316c
Frederic Pillon <frederic.pillon@st.com>
2025-09-01 14:22:39 +0200

    fix(stm32wrapper): header regex
    Fixes #2809.
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- libraries/SrcWrapper/inc/LL/stm32yyxx_ll_radio_timer.h
+++ libraries/SrcWrapper/inc/LL/stm32yyxx_ll_radio_timer.h
@@ -1,5 +1,5 @@
-#ifndef _STM32YYXX_LL_TIMER_H_
-#define _STM32YYXX_LL_TIMER_H_
+#ifndef _STM32YYXX_LL_RADIO_TIMER_H_
+#define _STM32YYXX_LL_RADIO_TIMER_H_
 /* LL raised several warnings, ignore them */
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
@@ -9,7 +9,7 @@
 #endif
 
 #ifdef STM32WB0x
-  #include "stm32wb0x_ll_timer.h"
+  #include "stm32wb0x_ll_radio_timer.h"
 #endif
 #pragma GCC diagnostic pop
-#endif /* _STM32YYXX_LL_TIMER_H_ */
+#endif /* _STM32YYXX_LL_RADIO_TIMER_H_ */
